8878b67408eb12eaeddbe3987e2e5cf64f563c67
Vladislav Zaborovskiy <vladzab@yandex.ru>
2016-08-25 13:18:49 +0300

    installation[started]

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # ck-tensorflow
 Collective Knowledge repository for evaluating and optimising performance of TensorFlow
+
+# Installing TensorFlow on Ubuntu
+
+## Dependencies
+
+### GPU
+
+If you want to use GPU, make sure you have installed CUDA toolkit >=7.0 and cuDNN v2+. If you want to use GPU and pip installation install CUDA toolkit 7.5 and cuDNN v4. 
+
+Check if you have [CUDA-enabled GPU](https://developer.nvidia.com/cuda-gpus).
+
+[Download and install CUDA](https://developer.nvidia.com/cuda-downloads).
+
+[Download and install cuDNN](https://developer.nvidia.com/rdp/cudnn-download) (Registration required)
+
+[More instructions](https://www.tensorflow.org/versions/r0.10/get_started/os_setup.html#optional-install-cuda-gpus-on-linux)
+
+### For pip installation
+
+For python 2.7 install
+```
+$ sudo apt-get install  \
+    python-dev \
+    python-pip
+```
+
+For python 3.5 install 
+
+```
+$ sudo apt-get install  \
+    python3-dev \
+    python3-pip
+```
---
+++ script/tf-pip-install/pip-install.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# PYTHON - python version 2 or 3
+# GPU-ENABLED - n or y
+
+
+#Getting python version from user
+DEFAULT="2"
+PYTHON="${PYTHON:-$DEFAULT}"
+read -e  -p "Choose python version (3/[2]): " PYTHON
+if [[ ($PYTHON != "2") && ($PYTHON != "3")]]
+then
+    echo "Error: version ${PYTHON} doesn't exist. Choose from [2/3]"
+    exit 1
+fi
+
+
+#Getting GPU_ENABLED answer
+echo ""
+DEFAULT="y"
+read -e  -p "GPU enabled? (n/[y]): " GPU_ENABLED
+GPU_ENABLED="${GPU_ENABLED:-$DEFAULT}"
+if [[ ($GPU_ENABLED != "n") && ($GPU_ENABLED != "y")]]
+then
+    echo "Error: GPU enabled answer is y or n."
+    exit 1
+fi
+
+
+echo "" 
+echo  "TensorFlow installation started"
+
+
+if [[ "$PYTHON" = "2" ]]
+then 
+    if [[ "$GPU_ENABLED" = "y" ]]
+    then
+         export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
+    else
+          export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
+    fi
+
+sudo pip install --upgrade $TF_BINARY_URL
+
+elif [[ "$PYTHON" -eq "3" ]]
+then
+    if [[ "$GPU_ENABLED" = "y" ]]
+    then
+        export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
+    else
+        export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
+    fi
+
+sudo pip3 install --upgrade $TF_BINARY_URL
+
+fi
+
