174f4e1be9f0dc5fe092ed339de637e55060dd20
Vladislav Zaborovskiy <vladzab@yandex.ru>
2016-08-30 13:31:16 +0300

    Installation[pip inst. finished]

---
+++ package/lib-tf-pip3/install.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+
+
+#Getting GPU_ENABLED answer
+echo ""
+DEFAULT="y"
+read -e  -p "GPU enabled? (n/[y]): " GPU_ENABLED
+GPU_ENABLED="${GPU_ENABLED:-$DEFAULT}"
+if [[ ($GPU_ENABLED != "n") && ($GPU_ENABLED != "y")]]
+then
+    echo "Error: GPU enabled answer is y or n."
+    exit 1
+fi
+
+
+
+if [[ "$GPU_ENABLED" = "y" ]]
+    then
+        export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
+    else
+        export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
+fi
+
+
+
+sudo pip3 install --upgrade $TF_BINARY_URL
+
