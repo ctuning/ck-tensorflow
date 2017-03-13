c88c44e072d023a97f5030c0621e93b47c775957
rhymmor <anatolybelonog@gmail.com>
2017-03-13 17:32:37 +0700

    added image, labels, model files flags for tf classification

--- program/tensorflow-classification-cpp/ck-run.sh
+++ program/tensorflow-classification-cpp/ck-run.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
+MODEL=$CK_ENV_LIB_TF_SRC/tensorflow/examples/label_image/data/tensorflow_inception_graph.pb
+LABELS=$CK_ENV_LIB_TF_SRC/tensorflow/examples/label_image/data/imagenet_comp_graph_label_strings.txt
+IMAGE=$CK_ENV_LIB_TF_SRC/tensorflow/examples/label_image/data/grace_hopper.jpg
+
+if [ ! -z $CK_TF_MODEL ]; then
+    MODEL=$CK_TF_MODEL
+fi
+if [ ! -z $CK_TF_LABELS ]; then
+    LABELS=$CK_TF_LABELS
+fi
+if [ ! -z $CK_TF_IMAGE ]; then
+    IMAGE=$CK_TF_IMAGE
+fi
+
+
 cd $CK_ENV_LIB_TF_SRC && bazel-bin/tensorflow/examples/label_image/label_image \
---graph=tensorflow/examples/label_image/data/tensorflow_inception_graph.pb \
---labels=tensorflow/examples/label_image/data/imagenet_comp_graph_label_strings.txt
+--graph=$MODEL \
+--labels=$LABELS \
+--image=$IMAGE
