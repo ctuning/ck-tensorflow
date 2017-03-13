4a63ee97d10e0e15dca6dc7c6b913caa02638ac0
rhymmor <anatolybelonog@gmail.com>
2017-03-13 19:12:11 +0700

    fixed used env

--- program/tensorflow-classification-cpp/ck-compile.sh
+++ program/tensorflow-classification-cpp/ck-compile.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
-TF_PATH=$CK_ENV_LIB_TF_SRC/tensorflow/examples/label_image
+TF_PATH=${CK_ENV_LIB_TF}/src/tensorflow/examples/label_image
 
-cp ../classification.cpp $TF_PATH/main.cc
+cp ../classification.cpp ${TF_PATH}/main.cc
 
-cd $CK_ENV_LIB_TF_SRC && bazel build tensorflow/examples/label_image
-
-if [ ! -f $TF_PATH/data/imagenet_comp_graph_label_strings.txt ] || \
-[ ! -f $TF_PATH/data/tensorflow_inception_graph.pb ]; then
-    echo "\n Downloading default labels and model files \n"
-    wget https://storage.googleapis.com/download.tensorflow.org/models/inception_dec_2015.zip -O \
-    $TF_PATH/data/inception_dec_2015.zip \
-    && unzip $TF_PATH/data/inception_dec_2015.zip -d $TF_PATH/data
+cd ${CK_ENV_LIB_TF}/src && bazel build tensorflow/examples/label_image 
+ 
+if [ ! -f ${TF_PATH}/data/imagenet_comp_graph_label_strings.txt ] || \ 
+[ ! -f ${TF_PATH}/data/tensorflow_inception_graph.pb ]; then 
+    echo "\n Downloading default labels and model files \n" 
+    wget https://storage.googleapis.com/download.tensorflow.org/models/inception_dec_2015.zip -O \ 
+    ${TF_PATH}/data/inception_dec_2015.zip \ 
+    && unzip ${TF_PATH}/data/inception_dec_2015.zip -d ${TF_PATH}/data 
 fi
+
+cd ${CK_ENV_LIB_TF}/src && bazel build tensorflow/examples/label_image
--- program/tensorflow-classification-cpp/ck-run.sh
+++ program/tensorflow-classification-cpp/ck-run.sh
@@ -4,18 +4,18 @@ MODEL=$CK_ENV_LIB_TF_SRC/tensorflow/examples/label_image/data/tensorflow_incepti
 LABELS=$CK_ENV_LIB_TF_SRC/tensorflow/examples/label_image/data/imagenet_comp_graph_label_strings.txt
 IMAGE=$CK_ENV_LIB_TF_SRC/tensorflow/examples/label_image/data/grace_hopper.jpg
 
-if [ ! -z $CK_TF_MODEL ]; then
-    MODEL=$CK_TF_MODEL
+if [ ! -z ${CK_TF_MODEL} ]; then
+    MODEL=${CK_TF_MODEL}
 fi
-if [ ! -z $CK_TF_LABELS ]; then
-    LABELS=$CK_TF_LABELS
+if [ ! -z ${CK_TF_LABELS} ]; then
+    LABELS=${CK_TF_LABELS}
 fi
-if [ ! -z $CK_TF_IMAGE ]; then
-    IMAGE=$CK_TF_IMAGE
+if [ ! -z ${CK_TF_IMAGE} ]; then
+    IMAGE=${CK_TF_IMAGE}
 fi
 
 
-cd $CK_ENV_LIB_TF_SRC && bazel-bin/tensorflow/examples/label_image/label_image \
---graph=$MODEL \
---labels=$LABELS \
---image=$IMAGE
+cd ${CK_ENV_LIB_TF}/src && bazel-bin/tensorflow/examples/label_image/label_image \
+--graph=${MODEL} \
+--labels=${LABELS} \
+--image=${IMAGE}
