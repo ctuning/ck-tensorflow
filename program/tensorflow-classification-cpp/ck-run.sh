2149fb127ac1d067e7fe1bd25ef6c080a099b599
rhymmor <anatolybelonog@gmail.com>
2017-03-14 16:59:37 +0700

    change default tf classification cpp model

--- program/tensorflow-classification-cpp/ck-run.sh
+++ program/tensorflow-classification-cpp/ck-run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-MODEL=$CK_ENV_MODEL_TENSORFLOW/classify_image_graph_def.pb
-LABELS=$CK_ENV_MODEL_TENSORFLOW/imagenet_synset_to_human_label_map.txt
+MODEL=$CK_ENV_DATASET_TENSORFLOW_AUX/tensorflow_inception_graph.pb
+LABELS=$CK_ENV_DATASET_TENSORFLOW_AUX/imagenet_comp_graph_label_strings.txt
 IMAGE=$CK_ENV_MODEL_TENSORFLOW/cropped_panda.jpg
 
 if [ ! -z ${CK_TF_MODEL} ]; then
@@ -14,7 +14,4 @@ if [ ! -z ${CK_TF_IMAGE} ]; then
 fi
 
 
-cd ${CK_ENV_LIB_TF}/src && bazel-bin/tensorflow/examples/label_image/label_image \
---graph=${MODEL} \
---labels=${LABELS} \
---image=${IMAGE}
+cd ${CK_ENV_LIB_TF}/src && bazel-bin/tensorflow/examples/label_image/label_image --graph=${MODEL} --labels=${LABELS} --image=${IMAGE}
