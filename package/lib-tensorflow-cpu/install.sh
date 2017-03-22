1827dbdfd2ad7280a04ccb54e33a60918a1fd1ce
Unknown <anatolybelonog@gmail.com>
2017-03-22 23:28:27 +0700

    fixed tf-classification compilation for android
    and added support for mac os as host with android target

--- package/lib-tensorflow-cpu/install.sh
+++ package/lib-tensorflow-cpu/install.sh
@@ -32,69 +32,73 @@ if [ "${?}" != "0" ] ; then
   exit 1
 fi
 
-######################################################################################
-cd $TENSORFLOW_SRC_DIR
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  #hack to make it work on mac os
+  cd ${TENSORFLOW_INSTALL_DIR} && mkdir -p lib/tensorflow && echo '' > lib/tensorflow/__init__.py
+else
+  ######################################################################################
+  cd ${TENSORFLOW_SRC_DIR}
 
-echo ""
-echo "Configuring ..."
+  echo ""
+  echo "Configuring ..."
 
-source ${PACKAGE_DIR}/export-variables
-./configure
-if [ "${?}" != "0" ] ; then
-  echo "Error: TensorFlow installation configuration failed!"
-  exit 1
+  source ${PACKAGE_DIR}/export-variables
+  ./configure
+  if [ "${?}" != "0" ] ; then
+    echo "Error: TensorFlow installation configuration failed!"
+    exit 1
+  fi
+
+  ######################################################################################
+  echo ""
+  echo "Preparing pip package ..."
+
+  if [ "$TF_NEED_CUDA" == 0 ]; then
+  if [ "$TF_NEED_OPENCL" == 0 ]; then
+    bazel build -c opt //tensorflow/tools/pip_package:build_pip_package;
+  else 
+    bazel build -c opt --config=sycl //tensorflow/tools/pip_package:build_pip_package;
+  fi
+  else 
+    bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package;
+  fi
+
+  if [ "${?}" != "0" ] ; then
+    echo "Error: Bazel building pip package failed"
+    exit 1
+  fi
+
+  ######################################################################################
+  echo ""
+  echo "Building pip package(s) ..."
+
+  bazel-bin/tensorflow/tools/pip_package/build_pip_package ${TENSORFLOW_PKG_DIR}
+  if [ "${?}" != "0" ] ; then
+    echo "Error: Bazel building pip package failed"
+    exit 1
+  fi
+
+  ######################################################################################
+  echo ""
+  echo "Installing pip package(s) ..."
+
+  for pip_package in ${TENSORFLOW_PKG_DIR}/*.whl
+  do
+      if [ "$CK_PYTHON3" == 0 ]; then
+          pip install $pip_package -t $TENSORFLOW_LIB_DIR
+          if [ "${?}" != "0" ] ; then
+              echo "Error: Bazel building pip package failed"
+              exit 1
+          fi
+      else
+          pip3 install $pip_package -t $TENSORFLOW_LIB_DIR
+          if [ "${?}" != "0" ] ; then
+              echo "Error: Bazel building pip package failed"
+              exit 1
+          fi
+      fi
+  done
 fi
-
-######################################################################################
-echo ""
-echo "Preparing pip package ..."
-
-if [ "$TF_NEED_CUDA" == 0 ]; then
- if [ "$TF_NEED_OPENCL" == 0 ]; then
-  bazel build -c opt //tensorflow/tools/pip_package:build_pip_package;
- else 
-   bazel build -c opt --config=sycl //tensorflow/tools/pip_package:build_pip_package;
- fi
-else 
-  bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package;
-fi
-
-if [ "${?}" != "0" ] ; then
-  echo "Error: Bazel building pip package failed"
-  exit 1
-fi
-
-######################################################################################
-echo ""
-echo "Building pip package(s) ..."
-
-bazel-bin/tensorflow/tools/pip_package/build_pip_package ${TENSORFLOW_PKG_DIR}
-if [ "${?}" != "0" ] ; then
-  echo "Error: Bazel building pip package failed"
-  exit 1
-fi
-
-######################################################################################
-echo ""
-echo "Installing pip package(s) ..."
-
-for pip_package in ${TENSORFLOW_PKG_DIR}/*.whl
-do
-    if [ "$CK_PYTHON3" == 0 ]; then
-        pip install $pip_package -t $TENSORFLOW_LIB_DIR
-        if [ "${?}" != "0" ] ; then
-            echo "Error: Bazel building pip package failed"
-            exit 1
-        fi
-    else
-        pip3 install $pip_package -t $TENSORFLOW_LIB_DIR
-        if [ "${?}" != "0" ] ; then
-            echo "Error: Bazel building pip package failed"
-            exit 1
-        fi
-    fi
-done
-
 ######################################################################################
 #echo ""
 #echo "Cleaning up directories ..."
--- program/tensorflow-classification-cpp/preprocess.py
+++ program/tensorflow-classification-cpp/preprocess.py
@@ -30,17 +30,30 @@ def ck_preprocess(i):
 
     # Find template
     x=deps['tensorflow-aux']
+    y=deps['tensorflow-model-imagenet']
 
-    pb_path_full=x['dict']['env']['CK_ENV_DATASET_TENSORFLOW_AUX'] + '/tensorflow_inception_graph.pb'
+    pb_path_full=x['dict']['env']['CK_ENV_DATASET_TENSORFLOW_AUX'] + '/tensorflow_inception_stripped.pb'
     nenv['CK_ENV_DATASET_TENSORFLOW_AUX_PB']=pb_path_full
+
+    txt_path_full=x['dict']['env']['CK_ENV_DATASET_TENSORFLOW_AUX'] + '/imagenet_comp_graph_label_strings.txt'
+    nenv['CK_ENV_DATASET_TENSORFLOW_AUX_TXT']=txt_path_full
+
+    img_path_full=y['dict']['env']['CK_ENV_MODEL_TENSORFLOW'] + '/cropped_panda.jpg'
+    nenv['CK_ENV_DATASET_TENSORFLOW_AUX_IMG']=img_path_full
 
     if remote=='yes':
        # For Android we need only filename without full path
        pb_path=os.path.basename(pb_path_full)
+       txt_path=os.path.basename(txt_path_full)
+       img_path=os.path.basename(img_path_full)
     else:
        pb_path=pb_path_full
+       txt_path=txt_path_full
+       img_path=img_path_full
 
     nenv['CK_DATASET_TENSORFLOW_AUX_PB']=pb_path
+    nenv['CK_DATASET_TENSORFLOW_AUX_TXT']=txt_path
+    nenv['CK_DATASET_TENSORFLOW_AUX_IMG']=img_path
 
     try:
         cur_dir=os.getcwd()
