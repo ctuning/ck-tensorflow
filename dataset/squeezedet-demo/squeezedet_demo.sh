570005ff61bfe9ee45289b2c868a52a51b3adb67
Anton Lokhmotov <anton@dividiti.com>
2017-03-22 18:58:28 +0000

    SqueezeDet demo script: apostrophes around path name, not within.

--- dataset/squeezedet-demo/squeezedet_demo.sh
+++ dataset/squeezedet-demo/squeezedet_demo.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-
-
-echo "Detecting objects in '${CK_ENV_MODEL_SQUEEZEDET_ROOT}'/data/sample.png"
-cd $CK_ENV_MODEL_SQUEEZEDET_ROOT
-$CK_ENV_COMPILER_PYTHON_FILE ./src/demo.py
-echo "Output in '${CK_ENV_MODEL_SQUEEZEDET_ROOT}'/data/out/"
+echo "Detecting objects in '${CK_ENV_MODEL_SQUEEZEDET_ROOT}/data/sample.png' ..."
+cd ${CK_ENV_MODEL_SQUEEZEDET_ROOT} && ${CK_ENV_COMPILER_PYTHON_FILE} ./src/demo.py
+echo "Check the output in '${CK_ENV_MODEL_SQUEEZEDET_ROOT}/data/out/' ..."
