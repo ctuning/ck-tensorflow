12e083ddaa25dc084c13421ece97da2986187a5d
Grigori Fursin <Grigori.Fursin@cTuning.org>
2017-03-27 17:44:15 +0100

    new clean up

--- program/tensorflow-classification-cpu/ck-compile.sh
+++ program/tensorflow-classification-cpu/ck-compile.sh
@@ -8,92 +8,55 @@ TENSORFLOW_SRC=${CK_ENV_LIB_TF}/src
 
 cd ${TENSORFLOW_SRC}
 
-MAKEFILE_DIR=tensorflow/contrib/makefile
-FULL_MAKEFILE_DIR=${TENSORFLOW_SRC}/${MAKEFILE_DIR}
+if [ "${CK_TARGET_OS_NAME2_ANDROID}" == "1" ] ; then
+  MAKEFILE_DIR=tensorflow/contrib/makefile
+  FULL_MAKEFILE_DIR=${TENSORFLOW_SRC}/${MAKEFILE_DIR}
 
-####################################################################
-if [ ! -d "${MAKEFILE_DIR}/downloads" ]; then
-  echo ""
-  echo "Downloading extra dependencies via TF ..."
-  echo ""
+  ####################################################################
+  if [ ! -d "${MAKEFILE_DIR}/downloads" ]; then
+    echo ""
+    echo "Downloading extra dependencies via TF ..."
+    echo ""
 
-  ${MAKEFILE_DIR}/download_dependencies.sh
-  if [ "${?}" != "0" ]; then
+    ${MAKEFILE_DIR}/download_dependencies.sh
+    if [ "${?}" != "0" ]; then
       echo ""
       echo "Error: Downloading dependencies for '${CK_ENV_LIB_TF}/src/${MAKEFILE_DIR}' failed!"
       exit 1
+    fi
   fi
-fi
 
-####################################################################
-cp -f ${PROGRAM_DIR}/classification.cpp ${MAKEFILE_DIR}/samples/classification.cc
-cp -f ${PROGRAM_DIR}/Makefile ${MAKEFILE_DIR}/Makefile
+  ####################################################################
+  cp -f ${PROGRAM_DIR}/classification.cpp ${MAKEFILE_DIR}/samples/classification.cc
+  cp -f ${PROGRAM_DIR}/Makefile ${MAKEFILE_DIR}/Makefile
 
-####################################################################
-if [ "${CK_TARGET_OS_NAME2_ANDROID}" == "1" ] ; then
-  PROTOBUF_INCLUDE=${CK_ENV_LIB_PROTOBUF_INCLUDE}
-else
-  PROTOBUF_INCLUDE=${CK_ENV_LIB_PROTOBUF_HOST_INCLUDE}
-fi
-
-####################################################################
-make -f ${MAKEFILE_DIR}/Makefile VERBOSE=1 CXX=${CK_CXX} PROTOBUF_INCLUDE=-I${PROTOBUF_INCLUDE} PROTOC=${CK_ENV_LIB_PROTOBUF_HOST_BIN}/protoc
-if [ "${?}" != "0" ] ; then
+  ####################################################################
+  make -f ${MAKEFILE_DIR}/Makefile VERBOSE=1 CXX=${CK_CXX} CK_INCLUDE="-I${CK_ENV_LIB_PROTOBUF_INCLUDE}" PROTOC=${CK_ENV_LIB_PROTOBUF_HOST_BIN}/protoc
+  if [ "${?}" != "0" ] ; then
     echo ""
     echo "Error: make for android classification failed!"
     exit 1
-fi
+  fi
 
-cp -f ${CK_ENV_LIB_TF}/src/${MAKEFILE_DIR}/gen/bin/benchmark ${TMP_DIR}/classification
+  cp -f ${CK_ENV_LIB_TF}/src/${MAKEFILE_DIR}/gen/bin/benchmark ${TMP_DIR}/classification
 
-
-exit 1
-
-if [ ! -z ${CK_ANDROID_NDK_PLATFORM} ]; then
-    #sudo apt-get install autoconf automake libtool curl make g++ unzip zlib1g-dev
-    export NDK_ROOT=${CK_ANDROID_NDK_ROOT_DIR}
-
-    #compile static library for libjpeg
-    if [ ! -f "${CK_LIB_LIBJPEG_TENSORFLOW}/obj/local/armeabi-v7a/libjpeg.a" ]; then
-        cd ${CK_LIB_LIBJPEG_TENSORFLOW} && ${NDK_ROOT}/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk APP_ABI=armeabi-v7a obj/local/armeabi-v7a/libjpeg.a
-        if [ "${?}" != "0" ] ; then
-            echo ""
-            echo "Error: Compiling static library for libjpeg failed!"
-            exit 1
-        fi
-    fi
-
-    cd ${CK_ENV_LIB_TF}/src
-    MAKEFILE_DIR=tensorflow/contrib/makefile
-    
-    
-    if [ ! -d "${MAKEFILE_DIR}/gen/protobuf" ]; then
-        tensorflow/contrib/makefile/compile_android_protobuf.sh -c
-        if [ "${?}" != "0" ] ; then
-            echo ""
-            echo "Error: Compiling android protobuf for '${CK_ENV_LIB_TF}/src/${MAKEFILE_DIR}' failed!"
-            exit 1
-        fi
-    fi
-
-    cp ${PROGRAM_DIR}/classification.cpp ${MAKEFILE_DIR}/samples/classification.cc
-    cp ${PROGRAM_DIR}/Makefile ${MAKEFILE_DIR}/Makefile
-    make -f ${MAKEFILE_DIR}/Makefile TARGET=ANDROID
-    if [ "${?}" != "0" ] ; then
-        echo ""
-        echo "Error: make for android classification failed!"
-        exit 1
-    fi
-
-    cp ${CK_ENV_LIB_TF}/src/${MAKEFILE_DIR}/gen/bin/benchmark ${TMP_DIR}/classification
 else
-    TF_PATH=${CK_ENV_LIB_TF}/src/tensorflow/examples/label_image
-    cp ../classification.cpp ${TF_PATH}/main.cc
-    cd ${CK_ENV_LIB_TF}/src && bazel build tensorflow/examples/label_image
-    if [ "${?}" != "0" ] ; then
-        echo ""
-        echo "Error: bazel build for classification failed!"
-        exit 1
-    fi
-    cp ${CK_ENV_LIB_TF}/src/bazel-bin/tensorflow/examples/label_image/label_image ${TMP_DIR}/classification
+  cp -f ${PROGRAM_DIR}/classification.cpp ${TENSORFLOW_SRC}/tensorflow/examples/label_image/main.cc
+
+#  Should be already done by CK tensorflow cpu package
+#  ./configure --prefix=${CK_ENV_LIB_TF}/install
+#  if [ "${?}" != "0" ] ; then
+#     echo ""
+#     echo "Error: configuration for TF failed!"
+#     exit 1
+#  fi
+
+  bazel build tensorflow/examples/label_image
+  if [ "${?}" != "0" ] ; then
+     echo ""
+     echo "Error: bazel build for classification failed!"
+     exit 1
+  fi
+
+  cp -f ${TENSORFLOW_SRC}/bazel-bin/tensorflow/examples/label_image/label_image ${TMP_DIR}/classification
 fi
--- program/tensorflow-classification-cpu/preprocess.py
+++ program/tensorflow-classification-cpu/preprocess.py
@@ -19,8 +19,7 @@ def ck_preprocess(i):
     remote=tosd.get('remote','')
 
     # Find template
-    x=deps['tensorflow-aux']
-    y=deps['tensorflow-model-imagenet']
+    x=deps['tensorflow-model']
 
     pb_path_full=x['dict']['env']['CK_ENV_DATASET_TENSORFLOW_STRIPPED_AUX'] + '/tensorflow_inception_stripped.pb'
     nenv['CK_ENV_DATASET_TENSORFLOW_AUX_PB']=pb_path_full
@@ -28,22 +27,16 @@ def ck_preprocess(i):
     txt_path_full=x['dict']['env']['CK_ENV_DATASET_TENSORFLOW_STRIPPED_AUX'] + '/imagenet_comp_graph_label_strings.txt'
     nenv['CK_ENV_DATASET_TENSORFLOW_AUX_TXT']=txt_path_full
 
-    img_path_full=y['dict']['env']['CK_ENV_MODEL_TENSORFLOW'] + '/cropped_panda.jpg'
-    nenv['CK_ENV_DATASET_TENSORFLOW_AUX_IMG']=img_path_full
-
     if remote=='yes':
        # For Android we need only filename without full path
        pb_path=os.path.basename(pb_path_full)
        txt_path=os.path.basename(txt_path_full)
-       img_path=os.path.basename(img_path_full)
     else:
        pb_path=pb_path_full
        txt_path=txt_path_full
-       img_path=img_path_full
 
     nenv['CK_DATASET_TENSORFLOW_AUX_PB']=pb_path
     nenv['CK_DATASET_TENSORFLOW_AUX_TXT']=txt_path
-    nenv['CK_DATASET_TENSORFLOW_AUX_IMG']=img_path
 
     return {'return':0, 'new_env':nenv}
 
