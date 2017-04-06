d18cc27364b885a70838bf0fcb4896a1d74e6860
Dmitry Savenko <dsavenko@xored.com>
2017-04-06 18:02:37 +0700

    Download model on Windows

--- package/tensorflowmodel-squeezedet/install.bat
+++ package/tensorflowmodel-squeezedet/install.bat
@@ -33,6 +33,7 @@ echo.
 echo "Extracting demo archive '%SQDT_DATA%/model_checkpoints.tar.gz' ..."
 
 cd "%SQDT_DATA%"
+
 gzip -d "model_checkpoints.tar.gz" 
 tar -xvf "model_checkpoints.tar"
 if %errorlevel% neq 0 (
@@ -46,68 +47,26 @@ rem ############################################################################
 echo.
 echo "Downloading pretrained CNN models to '%SQDT_DATA%/' ..."
 
-rem if [ "$SQUEEZENET_INCLUDED" != "0" ] ; then
-rem   echo "Downloading pretrained SqueezeNet CNN archive from '${CNN_SQUEEZENET_URL}' ..."
-rem   wget -P ${SQDT_DATA} ${CNN_SQUEEZENET_URL}
-rem   if [ "${?}" != "0" ] ; then
-rem     echo "Error: Downloading SqueezeNet CNN archive from '${CNN_SQUEEZENET_URL}' failed!"
-rem     echo "To skip downloading this CNN archive, set the 'SQUEEZENET_INCLUDED' parameter in 'tensorflowmodel-squeezedet/.cm/meta.json' to 0."
-rem     exit 1
-rem   fi
+if %SQUEEZENET_INCLUDED% neq 0 (
+  echo "Downloading pretrained SqueezeNet CNN archive from '%CNN_SQUEEZENET_URL%' ..."
+  wget --no-check-certificate -O "SqueezeNet.tar.gz" "%CNN_SQUEEZENET_URL%"
+  gzip -d "SqueezeNet.tar.gz" 
+  tar -xvf "SqueezeNet.tar"
+)
 
-rem   echo ""
-rem   echo "Extracting '${SQDT_DATA}'/SqueezeNet.tgz ..."
-rem   tar -xzvf ${SQDT_DATA}/SqueezeNet.tgz -C ${SQDT_DATA}/
-rem   if [ "${?}" != "0" ] ; then
-rem     echo "Error: Extracting CNN archive '${SQDT_DATA}'/SqueezeNet.tgz failed!"
-rem     exit 1
-rem   fi
+if %RESNET50_INCLUDED% neq 0 (
+  echo "Downloading pretrained ResNet50 CNN archive from '%CNN_RESNET50_URL%' ..."
+  wget --no-check-certificate -O "ResNet.tar.gz" "%CNN_RESNET50_URL%"
+  gzip -d "ResNet.tar.gz" 
+  tar -xvf "ResNet.tar"
+)
 
-rem   rm ${SQDT_DATA}/SqueezeNet.tgz
-rem fi
-
-
-rem if [ "$RESNET50_INCLUDED" != "0" ] ; then
-rem   echo ""
-rem   echo "Downloading pretrained ResNet50 CNN archive from '${CNN_RESNET50_URL}' ..."
-rem   wget -P ${SQDT_DATA} ${CNN_RESNET50_URL}
-rem   if [ "${?}" != "0" ] ; then
-rem     echo "Error: Downloading ResNet50 CNN archive from '${CNN_RESNET_URL}' failed!"
-rem     echo "To skip downloading this CNN archive, set the 'RESNET50_INCLUDED' parameter in 'tensorflowmodel-squeezedet/.cm/meta.json' to 0."
-rem     exit 1
-rem   fi
-
-rem   echo ""
-rem   echo "Extracting '${SQDT_DATA}'/ResNet.tgz ..."
-rem   tar -xzvf ${SQDT_DATA}/ResNet.tgz -C ${SQDT_DATA}/
-rem   if [ "${?}" != "0" ] ; then
-rem     echo "Error: Extracting CNN archive '${SQDT_DATA}'/ResNet.tgz failed!"
-rem     exit 1
-rem   fi
-
-rem   rm ${SQDT_DATA}/ResNet.tgz
-rem fi
-
-rem if [ "$VGG16_INCLUDED" != "0" ] ; then
-rem   echo ""
-rem   echo "Downloading pretrained VGG16 CNN archive from '${CNN_VGG16_URL}' ..."
-rem   wget -P ${SQDT_DATA} ${CNN_VGG16_URL}
-rem   if [ "${?}" != "0" ] ; then
-rem     echo "Error: Downloading VGG16 CNN archive from '${CNN_VGG16_URL}' failed!"
-rem     echo "To skip downloading this CNN archive, set the 'VGG16_INCLUDED' parameter in 'tensorflowmodel-squeezedet/.cm/meta.json' to 0."
-rem     exit 1
-rem   fi
-
-rem   echo ""
-rem   echo "Extracting '${SQDT_DATA}'/VGG16.tgz ..."
-rem   tar -xzvf ${SQDT_DATA}/VGG16.tgz -C ${SQDT_DATA}/
-rem   if [ "${?}" != "0" ] ; then
-rem     echo "Error: Extracting CNN archive '${SQDT_DATA}'/VGG16.tgz failed!"
-rem     exit 1
-rem   fi
-
-rem   rm ${SQDT_DATA}/VGG16.tgz
-rem fi
+if %VGG16_INCLUDED% neq 0 (
+  echo "Downloading pretrained VGG16 CNN archive from '%CNN_VGG16_URL%' ..."
+  wget --no-check-certificate -O "VGG16.tar.gz" "%CNN_VGG16_URL%"
+  gzip -d "VGG16.tar.gz" 
+  tar -xvf "VGG16.tar"
+)
 
 rem ######################################################################################
 rem # Some minor text editing to temporarily address point 1 in issue #10
