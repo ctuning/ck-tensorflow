7393bf83fa70281cbc23abfcb214ac6a54e97be4
Dmitry Savenko <dsavenko@xored.com>
2017-04-12 17:37:24 +0700

    Fix some stuff for Linux

--- program/squeezedet/continuous.sh
+++ program/squeezedet/continuous.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-ROOT=$CK_ENV_MODEL_SQUEEZEDET_ROOT
+ROOT=$PWD/..
 
 OUT_DIR="$PWD/out"
 rm -rf "$OUT_DIR"
@@ -9,6 +9,6 @@ export TF_CPP_MIN_LOG_LEVEL=3 # supress TF debug output
 
 IMAGES=$CK_ENV_DATASET_KITTI_IMAGE_DIR
 LABELS=$CK_ENV_DATASET_KITTI_LABELS_DIR
-CHECKPOINT="$ROOT/data/model_checkpoints/squeezeDet/model.ckpt-87000" # TODO: change this
+CHECKPOINT=$CK_ENV_MODEL_SQUEEZEDET_MODEL
 
 ${CK_ENV_COMPILER_PYTHON_FILE} "../continuous.py" --image_dir="$IMAGES" --label_dir="$LABELS" --out_dir="$OUT_DIR" --checkpoint="$CHECKPOINT"
