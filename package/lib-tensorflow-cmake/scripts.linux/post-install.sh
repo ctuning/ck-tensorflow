a0d11b53f5c69a233f9d33332a27ac27d05a66f8
Dmitry Savenko <dsavenko@xored.com>
2017-04-27 12:06:30 +0700

    Minor

--- package/lib-tensorflow-cmake/scripts.linux/post-install.sh
+++ package/lib-tensorflow-cmake/scripts.linux/post-install.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-${CK_MAKE} -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS} tf_python_build_pip_package
+${CK_MAKE} -j ${CK_HOST_CPU_NUMBER_OF_PROCESSORS:-1} tf_python_build_pip_package
 
 ${CK_ENV_COMPILER_PYTHON_FILE} -m pip install tf_python/dist/tensorflow-*.whl
