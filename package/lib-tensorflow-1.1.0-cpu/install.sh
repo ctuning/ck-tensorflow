aebd4e9cc9c7c55025a0b9409dbdbedc555ae204
Grigori Fursin <Grigori.Fursin@cTuning.org>
2017-05-15 16:40:24 +0100

    various fixes for Linux for 1.1.0 package

--- package/lib-tensorflow-1.1.0-cpu/install.sh
+++ package/lib-tensorflow-1.1.0-cpu/install.sh
@@ -3,7 +3,6 @@
 # CK installation script for TensorFlow package
 #
 # Developer(s):
-#  * Vladislav Zaborovskiy, vladzab@yandex.ru
 #  * Grigori Fursin, dividiti/cTuning foundation
 #
 
@@ -14,40 +13,37 @@
 export TENSORFLOW_LIB_DIR=${INSTALL_DIR}/lib
 
 ######################################################################################
-if [ "$HOSTTYPE" != "x86_64" ] ; then
-  echo "Error: this package only supports x86_64!"
-  exit 1
-fi
-
-######################################################################################
 echo ""
 echo "Removing '${TENSORFLOW_LIB_DIR}' ..."
 rm -rf ${TENSORFLOW_LIB_DIR}
 
 ######################################################################################
+# Print info about possible issues
 echo ""
-echo "Downloading and installing TensorFlow prebuilt binaries ..."
-# cd ${INSTALL_DIR}
+echo "Note that you sometimes need to upgrade your pip to the latest version"
+echo "to avoid well-known issues with user/system space installation:"
+echo ""
+echo "sudo ${CK_PYTHON_PIP_BIN} install --upgrade pip"
+echo ""
+read -p "Press enter to continue"
 
-if [[ $PYTHON3 == 0 ]]
-then
-  if [[ $GPU_ENABLED == 1 ]]
-  then
-    export TENSORFLOW_BIN_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-${TENSORFLOW_PACKAGE_VER}-cp27-none-linux_x86_64.whl
-  else
-    export TENSORFLOW_BIN_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-${TENSORFLOW_PACKAGE_VER}-cp27-none-linux_x86_64.whl
-  fi
-  pip install --upgrade ${TENSORFLOW_BIN_URL} -t ${TENSORFLOW_LIB_DIR} --trusted-host storage.googleapis.com
-else
-  if [[ $GPU_ENABLED == 1 ]]
-  then
-    export TENSORFLOW_BIN_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-${TENSORFLOW_PACKAGE_VER}-cp35-cp35m-linux_x86_64.whl
-  else
-    export TENSORFLOW_BIN_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-${TENSORFLOW_PACKAGE_VER}-cp35-cp35m-linux_x86_64.whl
-  fi
-  pip3 install --upgrade ${TENSORFLOW_BIN_URL} -t ${TENSORFLOW_LIB_DIR} --trusted-host storage.googleapis.com
+######################################################################################
+echo ""
+echo "Downloading and installing ProtoBuf (${PROTOBUF_PYTHON_URL}) ..."
+echo ""
+
+${CK_PYTHON_PIP_BIN} install --upgrade ${PROTOBUF_PYTHON_URL} -t ${TENSORFLOW_LIB_DIR} --trusted-host storage.googleapis.com --trusted-host pypi.python.org
+######################################################################################
+if [ "${?}" != "0" ] ; then
+  echo "Error: installation failed!"
+  exit 1
 fi
 
+echo ""
+echo "Downloading and installing TensorFlow prebuilt binaries (${TF_PYTHON_URL}) ..."
+echo ""
+
+${CK_PYTHON_PIP_BIN} install --upgrade ${TF_PYTHON_URL} -t ${TENSORFLOW_LIB_DIR} --trusted-host storage.googleapis.com
 ######################################################################################
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
