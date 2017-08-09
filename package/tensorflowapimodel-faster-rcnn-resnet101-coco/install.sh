4d467672de52a9955ab87511a3d7365dd1be6dd2
Vladislav Zaborovskiy <vladzab@yandex.ru>
2017-08-09 18:45:40 +0300

    Installation scripts fixes

--- package/tensorflowapimodel-faster-rcnn-resnet101-coco/install.sh
+++ package/tensorflowapimodel-faster-rcnn-resnet101-coco/install.sh
@@ -21,9 +21,9 @@ echo ""
 echo "Generating TFRecord files for training and validation... "
 
 #from $INSTALL_DIR/data
-$CK_PYTHON_BIN $PACKAGE_DIR/create_mscoco_record.py --label_map_path=$CK_ENV_TENSORFLOW_MODELS_ROOT/data/mscoco_label_map.pbtxt --data_dir=$CK_ENV_DATASET_COCO --output_dir=$DATA_DIR
+$CK_PYTHON_BIN $PACKAGE_DIR/create_mscoco_record.py --label_map_path=$CK_ENV_TENSORFLOW_MODELS_OBJ_DET_DIR/data/mscoco_label_map.pbtxt --data_dir=$CK_ENV_DATASET_COCO --output_dir=$DATA_DIR
 
-cp -f $CK_ENV_TENSORFLOW_MODELS_ROOT/data/mscoco_label_map.pbtxt $DATA_DIR
+cp -f $CK_ENV_TENSORFLOW_MODELS_OBJ_DET_DIR/data/mscoco_label_map.pbtxt $DATA_DIR
 
 if [ "${?}" != "0" ] ; then
   echo "Error: Generating TFRecord files for training and validation failed!"
