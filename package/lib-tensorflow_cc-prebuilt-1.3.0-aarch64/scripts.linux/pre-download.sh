b979f069e9392090252250632e6f3ba36960c3f2
Chunosov <N.Chunosov@yandex.ru>
2017-11-29 20:58:22 +0700

    TF_CC 1.3 prebuilt for 32-bit Linux (#34)
    * TF_CC prebuilt for 32-bit Linux
    
    * renaming

--- package/lib-tensorflow_cc-prebuilt-1.3.0-aarch64/scripts.linux/pre-download.sh
+++ package/lib-tensorflow_cc-prebuilt-1.3.0-aarch64/scripts.linux/pre-download.sh
@@ -14,3 +14,17 @@ if [ "$ARCH" != "aarch64" ]; then
     echo "Unsupported architecture. Only aarch64 is supported for now"
     exit 1
 fi
+
+ELF32=`file -L /sbin/init | grep -o "ELF 32-bit"`
+ELF64=`file -L /sbin/init | grep -o "ELF 64-bit"`
+
+if [ -n "$ELF32" ]; then
+    export PACKAGE_NAME="$PACKAGE_NAME_32_BIT"
+elif [ -n "$ELF64" ]; then
+    export PACKAGE_NAME="$PACKAGE_NAME_64_BIT"
+else
+    echo "Unsupported executable format. Only ELF 32-bit and ELF 64-bit are supported for now"
+    exit 1
+fi
+
+export PACKAGE_NAME1="$PACKAGE_NAME"
