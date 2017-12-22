a662aed286f7152f46059cee0019c6108145aa53
Grigori Fursin <Grigori.Fursin@cTuning.org>
2017-12-22 10:32:11 +0100

    fixed path for yad2k model when installed to env

--- package/model-yad2k/install.sh
+++ package/model-yad2k/install.sh
@@ -10,17 +10,19 @@
 # WEIGHTS_URL
 # CFG_URL
 
+INSTALL_DIR1=$INSTALL_DIR/src
+
 echo ""
-echo "Removing everything from $INSTALL_DIR..."
-rm -rf $INSTALL_DIR/*
+echo "Removing everything from $INSTALL_DIR1..."
+rm -rf $INSTALL_DIR1/*
 
 ######################################################################################
 echo ""
 echo "Downloading yad2k..."
-git clone $YAD2K_URL $INSTALL_DIR
+git clone $YAD2K_URL $INSTALL_DIR1
 if [ "${?}" != "0" ] ; then
     echo ""
-    echo "Error: Downloading to $INSTALL_DIR failed! Try to run installation script again"
+    echo "Error: Downloading to $INSTALL_DIR1 failed! Try to run installation script again"
     exit 1
 fi
 
@@ -29,13 +31,13 @@ fi
 echo ""
 echo "Downloading demo weights and model config..."
 if [ $INSTALL_DEMO == "yes" ] ; then
-    wget $WEIGHTS_URL -P $INSTALL_DIR
+    wget $WEIGHTS_URL -P $INSTALL_DIR1
     if [ "${?}" != "0" ] ; then
         echo ""
         echo "Error: Downloading demo weights from $WEIGHTS_URL failed!"
         exit 1
     fi
-    wget $CFG_URL -P $INSTALL_DIR
+    wget $CFG_URL -P $INSTALL_DIR1
     if [ "${?}" != "0" ] ; then
         echo ""
         echo "Error: Downloading demo configuration file from $CFG_URL failed!"
