6e60d13ddc974fe81727df899cb8fffbeb4f3d89
Anton Lokhmotov <anton@dividiti.com>
2017-12-22 09:39:19 +0000

    Save batch size as int.

--- program/squeezedet/postprocess_image_benchmark.py
+++ program/squeezedet/postprocess_image_benchmark.py
@@ -27,12 +27,13 @@ def ck_postprocess(i):
     d['env']={ var : env[var] for var in env }
 
     # Save metrics of interest.
+    d['batch_size']=int(d['env']['CK_BATCH_SIZE'])
     d['avg_fps']=1/d['avg_time_s']
     d['avg_time_ms']=d['avg_time_s']*1e3
-    d['total_time_ms']=d['avg_time_ms']*d['env']['CK_BATCH_SIZE']
+    d['total_time_ms']=d['avg_time_ms']*d['batch_size']
 
     # Save internal CK keys.
-    d['execution_time']=d['avg_time_s']*d['env']['CK_BATCH_SIZE']
+    d['execution_time']=d['avg_time_s']*d['batch_size']
     d['post_processed']='yes'
 
     # Save augmented output back to file.
