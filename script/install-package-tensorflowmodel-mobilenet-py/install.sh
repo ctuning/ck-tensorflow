b15cb8148c9f95ec062ab5ff86a8b74d21b79596
Nikolay Chunosov <nikolay.chunosov@xored.com>
2018-04-11 15:41:55 +0700

    mobilnet-v2 packages

--- script/install-package-tensorflowmodel-mobilenet-py/install.sh
+++ script/install-package-tensorflowmodel-mobilenet-py/install.sh
@@ -11,20 +11,44 @@
 # Python model and weights install script
 #
 
+MULTIPLIER=${MODEL_MOBILENET_MULTIPLIER}
+RESOLUTION=${MODEL_MOBILENET_RESOLUTION}
+VERSION=${MODEL_MOBILENET_VERSION}
+
+########################################################################
 echo
 echo "Download weights from ${PACKAGE_URL} ..."
 wget ${PACKAGE_URL}/${PACKAGE_NAME}
 
+########################################################################
 echo
 echo "Unpack weights file ${PACKAGE_NAME} ..."
 tar -zxvf ${PACKAGE_NAME}
 
+########################################################################
 echo
 echo "Remove temporary files ..."
 rm ${PACKAGE_NAME}
 
+# We don't use it right now, so remove to save disk space, but it can be useful in future
+if [ ${VERSION} == "2" ]; then
+  rm "mobilenet_v2_${MULTIPLIER}_${RESOLUTION}_eval.pbtxt"
+  rm "mobilenet_v2_${MULTIPLIER}_${RESOLUTION}_frozen.pb"
+  rm "mobilenet_v2_${MULTIPLIER}_${RESOLUTION}.tflite"
+fi
+
+########################################################################
 echo
 echo "Copy Python modules ..."
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 cp ${THIS_SCRIPT_DIR}/tf-mobilenet-model.py .
-cp ${THIS_SCRIPT_DIR}/mobilenet_v1.py .
+
+if [ ${VERSION} == "1" ]; then
+  cp ${THIS_SCRIPT_DIR}/mobilenet_v1.py .
+fi
+if [ ${VERSION} == "2" ]; then
+  cp ${THIS_SCRIPT_DIR}/mobilenet_v2.py .
+  cp ${THIS_SCRIPT_DIR}/mobilenet.py .
+  cp ${THIS_SCRIPT_DIR}/conv_blocks.py .
+fi
