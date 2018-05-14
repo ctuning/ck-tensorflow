e094a15b00d59228ef52191fb70930722375a79c
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-05-14 14:39:57 +0100

    an OSX-compatible version of prebuilt Tensorflow package

--- package/lib-tensorflow-1.4.0-cpu/install.sh
+++ package/lib-tensorflow-1.4.0-cpu/install.sh
@@ -8,14 +8,22 @@
 
 # PACKAGE_DIR
 # INSTALL_DIR
-# TENSORFLOW_URL
+# TF_PYTHON_URL
 
-export TENSORFLOW_LIB_DIR=${INSTALL_DIR}/lib
+
+    # This is where pip will install the modules.
+    # It has its own funny structure we don't control :
+    #
+EXTRA_PYTHON_SITE=${INSTALL_DIR}/python_deps_site
+
+SHORT_PYTHON_VERSION=`${CK_ENV_COMPILER_PYTHON_FILE} -c 'import sys;print(sys.version[:3])'`
+export PACKAGE_LIB_DIR="${EXTRA_PYTHON_SITE}/lib/python${SHORT_PYTHON_VERSION}/site-packages"
+export PYTHONPATH=$PACKAGE_LIB_DIR:$PYTHONPATH
 
 ######################################################################################
 echo ""
-echo "Removing '${TENSORFLOW_LIB_DIR}' ..."
-rm -rf ${TENSORFLOW_LIB_DIR}
+echo "Removing '${EXTRA_PYTHON_SITE}' ..."
+rm -rf ${EXTRA_PYTHON_SITE}
 
 ######################################################################################
 # Print info about possible issues
@@ -23,30 +31,9 @@ echo ""
 echo "Note that you sometimes need to upgrade your pip to the latest version"
 echo "to avoid well-known issues with user/system space installation:"
 
-SUDO="sudo "
-if [[ ${CK_PYTHON_PIP_BIN_FULL} == /home/* ]] ; then
-  SUDO=""
-fi
-
-#echo ""
-#read -r -p "Upgrade pip and install other deps (Y/n)? " x
-#
-#case "$x" in
-#  [nN][oO]|[nN])
-#    ;;
-#  *)
-#    echo ""
-#    echo "Using ${SUDO} ${CK_PYTHON_PIP_BIN_FULL} ..."
-#    echo ""
-#
-#    ${SUDO} ${CK_PYTHON_PIP_BIN_FULL} install --upgrade pip
-#    ${SUDO} ${CK_PYTHON_PIP_BIN_FULL} install protobuf easydict joblib image numpy scipy enum-compat
-#    ${SUDO} ${CK_PYTHON_PIP_BIN_FULL} uninstall enum34
-#    ;;
-#esac
-
+######################################################################################
 # Check if has --system option
-${CK_PYTHON_PIP_BIN_FULL} install --help > tmp-pip-help.tmp
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install --help > tmp-pip-help.tmp
 if grep -q "\-\-system" tmp-pip-help.tmp ; then
  SYS=" --system"
 fi
@@ -56,31 +43,47 @@ rm -f tmp-pip-help.tmp
 echo "Downloading and installing deps ..."
 echo ""
 
-${CK_PYTHON_PIP_BIN_FULL} install --upgrade pip protobuf easydict joblib image numpy scipy enum-compat -t ${TENSORFLOW_LIB_DIR}  ${SYS}
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install protobuf easydict joblib image numpy scipy enum-compat --prefix=${EXTRA_PYTHON_SITE}  ${SYS}
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
   exit 1
 fi
 
-${CK_PYTHON_PIP_BIN_FULL} uninstall enum34 ${SYS}
-# enum34 is not always installed so skip check for error - it doesn't matter ...
-#if [ "${?}" != "0" ] ; then
-#  echo "Error: installation failed!"
-#  exit 1
-#fi
+######################################################################################
+MAJOR_PYTHON_VERSION=`${CK_ENV_COMPILER_PYTHON_FILE} -c 'import sys;print(sys.version[0])'`
+
+if [ "$MAJOR_PYTHON_VERSION" == 3 ]; then
+    echo ""
+    echo "Conditionally cleaning up the 'enum34' package..."
+    ${CK_ENV_COMPILER_PYTHON_FILE} -m pip uninstall enum34 ${SYS}
+        #
+        # enum34 may not have been installed, so we do not check the return code
+        #
+    echo ""
+fi
 
 ######################################################################################
-echo ""
 echo "Downloading and installing TensorFlow prebuilt binaries (${TF_PYTHON_URL}) ..."
 echo ""
 
-${CK_PYTHON_PIP_BIN_FULL} install --upgrade ${TF_PYTHON_URL} -t ${TENSORFLOW_LIB_DIR} --trusted-host storage.googleapis.com ${SYS}
+${CK_ENV_COMPILER_PYTHON_FILE} -m pip install ${TF_PYTHON_URL} --prefix=${EXTRA_PYTHON_SITE}  --trusted-host storage.googleapis.com  ${SYS}
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
   exit 1
 fi
 
-cd ${INSTALL_DIR}/lib
-rm -rf enum
+######################################################################################
 
-exit 0
+if [ "$MAJOR_PYTHON_VERSION" == 3 ]; then
+    echo "Force clean up the 'enum34' package that we could have installed in our EXTRA_PYTHON_SITE"
+    echo ""
+
+    cd ${PACKAGE_LIB_DIR}
+    rm -rf enum*
+fi
+
+    # Because we have to provide a fixed name via meta.json ,
+    # and $PACKAGE_LIB_DIR depends on the Python version,
+    # we solve it by creating a symbolic link with a fixed name.
+    #
+ln -s $PACKAGE_LIB_DIR ${INSTALL_DIR}/lib
