b1860f45714aec433521cc3c81f6237495c02720
Nikolay Chunosov <nikolay.chunosov@xored.com>
2018-05-15 18:21:22 +0700

    preprocess images

--- program/classification-tensorflow-cpp/classification.cpp
+++ program/classification-tensorflow-cpp/classification.cpp
@@ -1,10 +1,14 @@
-#include "tensorflow/core/public/session.h" 
 #include <dirent.h>
-#include <chrono>  
+#include <chrono>
+
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/cc/framework/scope.h"
+//#include "tensorflow/cc/ops/standard_ops.h"
 
 using namespace std;
 
 using tensorflow::Status;
+using tensorflow::Tensor;
 
 inline int getenv_i(const char *name, int def) {
   return getenv(name) ? atoi(getenv(name)) : def;
@@ -16,8 +20,59 @@ string image_file(getenv("CK_IMAGE_FILE"));
 int batch_count = getenv_i("CK_BATCH_COUNT", 1);
 int batch_size = getenv_i("CK_BATCH_SIZE", 1);
 int image_count = batch_count * batch_size;
+int image_size = getenv_i("CK_ENV_TENSORFLOW_MODEL_IMAGE_HEIGHT", 224);
+float input_mean = 128.0f;
+float input_std = 128.0f;
 
+const int DT_FLOAT = 1;
 
+/*
+// Given an image file name, read in the data, try to decode it as an image,
+// resize it to the requested size, and then scale the values as desired.
+Status ReadTensorFromImageFile(string file_name, const int input_height,
+                               const int input_width, const float input_mean,
+                               const float input_std,
+                               std::vector<Tensor>* out_tensors) {
+  auto root = tensorflow::Scope::NewRootScope();
+  using namespace   tensorflow::ops;  // NOLINT(build/namespaces)
+
+  string input_name = "file_reader";
+  string output_name = "normalized";
+  auto file_reader = tensorflow::ops::ReadFile(root.WithOpName(input_name),
+                                               file_name);
+  // Now try to figure out what kind of file it is and decode it.
+  const int wanted_channels = 3;
+  tensorflow::Output image_reader;
+  image_reader = DecodeJpeg(root.WithOpName("jpeg_reader"), file_reader,
+                            DecodeJpeg::Channels(wanted_channels));
+  // Now cast the image data to float so we can do normal math on it.
+  auto float_caster =
+      Cast(root.WithOpName("float_caster"), image_reader, tensorflow::DT_FLOAT);
+  // The convention for image ops in TensorFlow is that all images are expected
+  // to be in batches, so that they're four-dimensional arrays with indices of
+  // [batch, height, width, channel]. Because we only have a single image, we
+  // have to add a batch dimension of 1 to the start with ExpandDims().
+  auto dims_expander = ExpandDims(root, float_caster, 0);
+  // Bilinearly resize the image to fit the required dimensions.
+  auto resized = ResizeBilinear(
+      root, dims_expander,
+      Const(root.WithOpName("size"), {input_height, input_width}));
+  // Subtract the mean and divide by the scale.
+  Div(root.WithOpName(output_name), Sub(root, resized, {input_mean}),
+      {input_std});
+
+  // This runs the GraphDef network definition that we've just constructed, and
+  // returns the results in the output tensor.
+  tensorflow::GraphDef graph;
+  TF_RETURN_IF_ERROR(root.ToGraphDef(&graph));
+
+  std::unique_ptr<tensorflow::Session> session(
+      tensorflow::NewSession(tensorflow::SessionOptions()));
+  TF_RETURN_IF_ERROR(session->Create(graph));
+  TF_RETURN_IF_ERROR(session->Run({}, {output_name}, {}, out_tensors));
+  return Status::OK();
+}
+*/
 // Reads a model graph definition from disk, and creates a session object you
 // can use to run it.
 Status LoadGraph(string graph_file_name,
@@ -58,6 +113,15 @@ vector<string> get_image_list() {
   return img_files;
 }
 
+template <typename TData>
+inline TData *get_random_raw_data(int data_count) {
+  const int rnd_max = 1000000;
+  const int rnd_range = 2 * rnd_max + 1;
+  TData *data = new TData[data_count];
+  for (int i = 0; i < data_count; ++i)
+    data[i] = static_cast<TData>(-rnd_max + rand() % rnd_range) / static_cast<TData>(rnd_max);
+  return data;
+}
 
 int main(int argc, char* argv[]) {
   cout << "Graph file: " << graph_file << endl;
@@ -81,18 +145,32 @@ int main(int argc, char* argv[]) {
   string graph_path = weights_dir + '/' + graph_file;
   Status load_graph_status = LoadGraph(graph_path, &session);
   if (!load_graph_status.ok()) {
-    LOG(ERROR) << load_graph_status;
+    LOG(ERROR) << "Loading graph failed: " << load_graph_status;
     return -1;
   }
   auto finish_time = chrono::high_resolution_clock::now(); 
   std::chrono::duration<double> elapsed = finish_time - start_time;
   cout << "Graph loaded in " << elapsed.count() << "s" << endl;
+/*
+  auto image_files = get_image_list();
+  cout << "Images count: " << image_files.size() << endl;
+  for (auto image_files : image_files)
+    cout << image_files << endl;
 
-  auto img_files = get_image_list();
-  cout << "Images count: " << img_files.size() << endl;
-  for (auto img_file : img_files)
-    cout << img_file << endl;
-  return 0;
+  auto image_file = image_files[0];
+*/
+  // Get the image from disk as a float array of numbers, resized and normalized
+  // to the specifications the main graph expects.
+  /*std::vector<Tensor> resized_tensors;
+  string image_path = image_dir + '/' + image_file;
+  Status read_tensor_status = ReadTensorFromImageFile(
+    image_path, image_size, image_size, input_mean, input_std, &resized_tensors);
+  if (!read_tensor_status.ok()) {
+    LOG(ERROR) << read_tensor_status;
+    return -1;
+  }
+  const Tensor& resized_tensor = resized_tensors[0];*/
+  
 /*
   tensorflow::SessionOptions options; 
   tensorflow::Session* session_pointer = nullptr;
@@ -114,6 +192,27 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 */
+  float *in_data = get_random_raw_data<float>(2* 224 * 224 * 3);
+  Tensor input(static_cast<tensorflow::DataType>(DT_FLOAT), tensorflow::TensorShape({2, 224, 224, 3}));
+  memcpy(input.flat<float>().data(), in_data, 2 * 224 * 224 * 3 * sizeof(float));
+  delete[] in_data;
+
+  vector<Tensor> outputs;
+  Status run_status = session->Run({{"input", input}},
+                                   {"MobilenetV1/Predictions/Reshape_1"}, {}, &outputs);
+  if (!run_status.ok()) {
+    LOG(ERROR) << "Running model failed: " << run_status;
+    return -1;
+  }
+
+  auto output = outputs[0];
+
+  int out_num = output.shape().dim_size(0);
+  int out_height = output.shape().dim_size(1);
+  int out_width = output.shape().dim_size(2);
+  int out_channels = output.shape().dim_size(3);
+  cout << "Output shape: " << out_num << " x "<< out_height << " x "<< out_width << " x "<< out_channels << endl;
+
 
   return 0;
 }
---
+++ program/classification-tensorflow-cpp/preprocess.py
@@ -0,0 +1,187 @@
+#
+# Copyright (c) 2018 cTuning foundation.
+# See CK COPYRIGHT.txt for copyright details.
+#
+# SPDX-License-Identifier: BSD-3-Clause.
+# See CK LICENSE.txt for licensing details.
+#
+
+import imp
+import os
+import re
+import json
+import shutil
+import numpy as np
+import scipy.io
+from scipy.ndimage import zoom
+
+# Zoom to target size
+def resize_img(img, target_size):
+  zoom_w = float(target_size)/float(img.shape[0])
+  zoom_h = float(target_size)/float(img.shape[1])
+  return zoom(img, [zoom_w, zoom_h, 1])
+
+
+# Crop the central region of the image
+def crop_img(img, crop_percent):
+  if crop_percent > 0 and crop_percent < 1.0:
+    new_w = int(img.shape[0] * crop_percent)
+    new_h = int(img.shape[1] * crop_percent)
+    offset_w = int((img.shape[0] - new_w)/2)
+    offset_h = int((img.shape[1] - new_h)/2)
+    return img[offset_w:new_w+offset_w, offset_h:new_h+offset_h, :]
+  else:
+    return img
+
+
+def ck_preprocess(i):
+  print('\n--------------------------------')
+  def my_env(var): return i['env'][var]
+  def dep_env(dep, var): return i['deps'][dep]['dict']['env'][var]
+
+  # Init variables from environment
+  MODEL_WEIGHTS = dep_env('weights', 'CK_ENV_TENSORFLOW_MODEL_WEIGHTS')
+  MODEL_DIR, _ = os.path.split(MODEL_WEIGHTS)
+  MODEL_FROZEN_FILE = dep_env('weights', 'CK_ENV_TENSORFLOW_MODEL_FROZEN_FILE')
+  assert MODEL_FROZEN_FILE, "Frozen graph is not found in selected model package"
+  MODEL_FROZEN_GRAPH_PATH = os.path.join(MODEL_DIR, MODEL_FROZEN_FILE)
+  MODEL_IMAGE_WIDTH = int(dep_env('weights', "CK_ENV_TENSORFLOW_MODEL_IMAGE_WIDTH"))
+  MODEL_IMAGE_HEIGHT = int(dep_env('weights', "CK_ENV_TENSORFLOW_MODEL_IMAGE_HEIGHT"))
+  assert MODEL_IMAGE_WIDTH == MODEL_IMAGE_HEIGHT, "Only square images are supported at this time"
+  IMAGE_SIZE = MODEL_IMAGE_WIDTH
+  MODEL_NORMALIZE_DATA = dep_env('weights', "CK_ENV_TENSORFLOW_MODEL_NORMALIZE_DATA") == "YES"
+  MODEL_CONVERT_TO_BGR = dep_env('weights', "CK_ENV_TENSORFLOW_MODEL_CONVERT_TO_BGR") == "YES"
+  BATCH_COUNT = int(my_env('CK_BATCH_COUNT'))
+  BATCH_SIZE = int(my_env('CK_BATCH_SIZE'))
+  IMAGES_COUNT = BATCH_COUNT * BATCH_SIZE
+  SKIP_IMAGES = int(my_env('CK_SKIP_IMAGES'))
+  IMAGE_DIR = dep_env('imagenet-val', 'CK_ENV_DATASET_IMAGENET_VAL')
+  MODE_SUFFIX = '-{}-{}-{}'.format(IMAGE_SIZE, BATCH_SIZE, BATCH_COUNT)
+  RESULTS_DIR = my_env('CK_RESULTS_DIR')
+
+  # Preprocessing options:
+  #
+  # CK_TMP_IMAGE_SIZE - if this set and greater tnan IMAGE_SIZE
+  #                     then images will be scaled to this size
+  #                     and then cropped to target size
+  # CK_CROP_PERCENT   - if TMP_IMAGE_SIZE is not set,
+  #                     images will be cropped to this percent
+  #                     and then scaled to target size
+  #
+  TMP_IMAGE_SIZE = int(my_env('CK_TMP_IMAGE_SIZE'))
+  CROP_PERCENT = float(my_env('CK_CROP_PERCENT'))
+
+  # Dir for cached prepared images
+  CACHE_DIR_ROOT = my_env("CK_CACHE_DIR")
+  CACHE_DIR = os.path.join(CACHE_DIR_ROOT, '{}-{}-{}'.format(IMAGE_SIZE, TMP_IMAGE_SIZE, CROP_PERCENT))
+  RECREATE_CACHE = my_env("CK_RECREATE_CACHE") == "YES"
+
+  print('Frozen graph: ' + MODEL_FROZEN_GRAPH_PATH)
+  print('Image size: {}x{}'.format(MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH))
+  print('Input images dir: ' + IMAGE_DIR)
+  print('Batch size: %d' % BATCH_SIZE)
+  print('Batch count: %d' % BATCH_COUNT)
+  print('Results dir: {}'.format(RESULTS_DIR))
+  print('Preprocessed images dir: {}'.format(CACHE_DIR))
+  print('Skip images: {}'.format(SKIP_IMAGES))
+
+  # Prepare cache dir
+  if not os.path.isdir(CACHE_DIR_ROOT):
+    os.mkdir(CACHE_DIR_ROOT)
+  if RECREATE_CACHE:
+    if os.path.isdir(CACHE_DIR):
+      shutil.rmtree(CACHE_DIR) 
+  if not os.path.isdir(CACHE_DIR):
+    os.mkdir(CACHE_DIR)
+
+  # Prepare results directory
+  if os.path.isdir(RESULTS_DIR):
+    shutil.rmtree(RESULTS_DIR)
+  os.mkdir(RESULTS_DIR)
+
+
+  # Load list of images to be processed
+  def load_image_list():
+    assert os.path.isdir(IMAGE_DIR), 'Input dir does not exit'
+    files = [f for f in os.listdir(IMAGE_DIR) if os.path.isfile(os.path.join(IMAGE_DIR, f))]
+    files = [f for f in files if re.search(r'\.jpg$', f, re.IGNORECASE)
+                              or re.search(r'\.jpeg$', f, re.IGNORECASE)]
+    assert len(files) > 0, 'Input dir does not contain image files'
+    files = sorted(files)[SKIP_IMAGES:]
+    assert len(files) > 0, 'Input dir does not contain more files'
+    images = files[:IMAGES_COUNT]
+    if len(images) < IMAGES_COUNT:
+      for _ in range(IMAGES_COUNT-len(images)):
+        images.append(images[-1])
+    return images
+
+
+  # Load processing image filenames
+  image_list = load_image_list()
+
+
+  # Returns path to preprocessed image in cache directory
+  def get_cached_path(image_file_name):
+    return os.path.join(CACHE_DIR, image_file_name) + '.npy'
+
+
+  # Returns path to source image in dataset directory
+  def get_original_path(image_file_name):
+    return os.path.join(IMAGE_DIR, image_file_name)
+
+
+  # Load and preprocess image
+  def load_image(image_path):
+    img = scipy.misc.imread(image_path)
+
+    # check if grayscale and convert to RGB
+    if len(img.shape) == 2:
+        img = np.dstack((img,img,img))
+
+    # drop alpha-channel if present
+    if img.shape[2] > 3:
+        img = img[:,:,:3]
+
+    # Resize and crop
+    if TMP_IMAGE_SIZE > IMAGE_SIZE:
+      img = resize_img(img, TMP_IMAGE_SIZE)
+      img = crop_img(img, float(IMAGE_SIZE)/float(TMP_IMAGE_SIZE))
+    else:
+      img = crop_img(img, CROP_PERCENT/100.0)
+      img = resize_img(img, IMAGE_SIZE)
+
+    # Convert to BGR
+    if MODEL_CONVERT_TO_BGR:
+      swap_img = np.array(img)
+      tmp_img = np.array(swap_img)
+      tmp_img[:, :, 0] = swap_img[:, :, 2]
+      tmp_img[:, :, 2] = swap_img[:, :, 0]
+      img = tmp_img
+
+    # Convert to float
+    img = img.astype(np.float)
+
+    # Normalize
+    if MODEL_NORMALIZE_DATA:
+      img = img / 255.0
+      img = img - 0.5
+      img = img * 2
+
+    return img
+
+
+  print('Preprocess images...')
+  for image_file in image_list:
+    if os.path.isfile(get_cached_path(image_file)):
+      continue
+    print(image_file)
+    image_data = load_image(get_original_path(image_file))
+    np.save(get_cached_path(image_file), image_data)
+
+  # Setup parameters to program
+  os.putenv('RUN_OPT_IMAGES_DIR', CACHE_DIR)
+  os.putenv('RUN_OPT_BATCH_SIZE', BATC)
+
+  print('--------------------------------\n')
+  return {'return': 0}
+
