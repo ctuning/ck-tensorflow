97ed2790be9a19b143ff94d4a32ad1721b9fcb23
Nikolay Chunosov <nikolay.chunosov@xored.com>
2018-06-08 17:58:15 +0700

    tflite classification program

--- program/tensorflow-lite-classification/classify.cpp
+++ program/tensorflow-lite-classification/classify.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <cstdlib>
 
 #include "tensorflow/contrib/lite/kernels/register.h"
 #include "tensorflow/contrib/lite/model.h"
 #include "tensorflow/contrib/lite/examples/label_image/get_top_n_impl.h"
+
+#ifdef XOPENME
+#include <xopenme.h>
+#endif
 
 #include "image_helper.h"
 
@@ -28,17 +33,26 @@ void check_file(const string& path, const string& id) {
 
 int main(int argc, char *argv[]) {
   try {
+#ifdef XOPENME
+    xopenme_init(3,0);
+#endif
+
+#ifdef XOPENME
+    xopenme_clock_start(0);
+#endif
     // Parse command line arguments
     string image_file;
     string model_file;
     string labels_file;
-    for (int i = 1; i < argc; i++)
+    for (int i = 1; i < argc; i++) {
+      cout << "ARG " << argv[i] << endl;
       get_arg(argv[i], "--image=", image_file) ||
       get_arg(argv[i], "--graph=", model_file) ||
       get_arg(argv[i], "--labels=", labels_file);
+    }
     check_file(image_file, "Image");
-    //check_file(model_file, "Model");
-    //check_file(labels_file, "Labels");
+    check_file(model_file, "Model");
+    check_file(labels_file, "Labels");
 
     // Read input image
     ImageData img_data = load_jpeg_file(image_file);
@@ -48,105 +62,85 @@ int main(int argc, char *argv[]) {
     if (img_data.channels != 3)
       throw string("Only RGB images are supported");
 
-    // Resize input image to model
-    const int model_image_H = 224;
-    const int model_image_W = 224;
-    vector<float> img_data_resized = resize_image(img_data, model_image_H, model_image_W);
+    // Load network from ftlite file
+    unique_ptr<tflite::FlatBufferModel> model;
+    model = tflite::FlatBufferModel::BuildFromFile(model_file.c_str());
+    if (!model)
+      throw "Failed to load model " + model_file;
+
+    unique_ptr<tflite::Interpreter> interpreter;
+    tflite::ops::builtin::BuiltinOpResolver resolver;
+    tflite::InterpreterBuilder(*model, resolver)(&interpreter);
+    if (!interpreter)
+      throw string("Failed to construct interpreter");
+    if (interpreter->AllocateTensors() != kTfLiteOk)
+      throw string("Failed to allocate tensors");
+      
+    int input = interpreter->inputs()[0];
+    int output = interpreter->outputs()[0];
+    if (interpreter->tensor(input)->type != kTfLiteFloat32 ||
+        interpreter->tensor(output)->type != kTfLiteFloat32)
+      throw string("This demo is for FLOAT input/output only");
+
+    // Prepare input image
+    TfLiteIntArray* dims = interpreter->tensor(input)->dims;
+    int wanted_height = dims->data[1];
+    int wanted_width = dims->data[2];
+    int wanted_channels = dims->data[3];
+    if (wanted_channels != img_data.channels)
+      throw string("Unsupported channels number in model");
+    resize_image(interpreter->typed_tensor<float>(input), img_data, wanted_height, wanted_width);
+#ifdef XOPENME
+    xopenme_clock_end(0);
+#endif
+
+    // Classify image
+    long ct_repeat_max = getenv("CT_REPEAT_MAIN") ? atol(getenv("CT_REPEAT_MAIN")) : 1;
+#ifdef XOPENME
+    xopenme_clock_start(2);
+#endif
+    for (int i = 0; i < ct_repeat_max; i++) {
+      if (interpreter->Invoke() != kTfLiteOk)
+        throw string("Failed to invoke tflite");
+    }
+#ifdef XOPENME
+    xopenme_clock_end(2);
+#endif
+    cout << "OK: Image classified" << endl;
+
+    // Process results
+    const int output_size = 1000;
+    const size_t num_results = 5;
+    const float threshold = 0.0001f;
+    vector<pair<float, int>> top_results;
+    tflite::label_image::get_top_n<float>(
+      interpreter->typed_output_tensor<float>(0),
+      output_size, num_results, threshold, &top_results, false);
+
+    // Read labels
+    vector<string> labels;
+    ifstream file(labels_file);
+    string line;
+    while (getline(file, line))
+      labels.push_back(line);
+
+    // Print predictions
+    cout << "---------- Prediction for " << image_file << " ----------" << endl;
+    for (const auto& result : top_results) {
+      const float confidence = result.first;
+      const int index = result.second;
+      cout << fixed << setprecision(4) << confidence 
+        << " - \"" << labels[index] << " (" << index << ")\"" << endl;
+    }
+
+#ifdef XOPENME
+    xopenme_dump_state();
+    xopenme_finish();
+#endif    
   }
   catch (const string& error_message) {
     cout << "ERROR: " << error_message << endl;
     return -1;
   }
-
-  /*tflite::label_image::Settings s;
-  s.model_name = getenv("RUN_OPT_MODEL");
-  s.input_bmp_name = getenv("RUN_OPT_IMAGE");
-  s.labels_file_name = getenv("RUN_OPT_LABELS");
-
-  // Load network from ftlite file
-  unique_ptr<tflite::FlatBufferModel> model;
-  model = tflite::FlatBufferModel::BuildFromFile(s.model_name.c_str());
-  if (!model) {
-    cerr << "ERROR: Failed to load model " << s.model_name << endl;
-    return -1;
-  }
-  cout << "OK: Model loaded: " << s.model_name << endl;
-*/
-  // Build interpter
-/*  unique_ptr<tflite::Interpreter> interpreter;
-  tflite::ops::builtin::BuiltinOpResolver resolver;
-  tflite::InterpreterBuilder(*model, resolver)(&interpreter);
-  if (!interpreter) {
-    cerr << "ERROR: Failed to construct interpreter" << endl;
-    return -1;
-  }
-  cout << "OK: Interpreter constructed" << endl;
-  interpreter->UseNNAPI(s.accel);
-  interpreter->SetNumThreads(s.number_of_threads);
-
-  // Load test input image
-  int image_width = 224;
-  int image_height = 224;
-  int image_channels = 3;
-  uint8_t* input_data = tflite::label_image::read_bmp(
-    s.input_bmp_name.c_str(), &image_width, &image_height, &image_channels, &s);
-  cout << "OK: Input image loaded: " << s.input_bmp_name << endl;
-  int input = interpreter->inputs()[0];
-  int output = interpreter->outputs()[0];
-  if (interpreter->tensor(input)->type != kTfLiteUInt8 ||
-      interpreter->tensor(output)->type != kTfLiteUInt8) {
-    cerr << "ERROR: this demo is for UINT8 input/output only" << endl;
-    return -1;
-  }
-
-  // Allocate memory
-  const vector<int> inputs = interpreter->inputs();
-  const vector<int> outputs = interpreter->outputs();
-  if (interpreter->AllocateTensors() != kTfLiteOk) {
-    cerr << "ERROR: Failed to allocate tensors" << endl;
-    return -1;
-  }
-
-  // Prepare input image
-  TfLiteIntArray* dims = interpreter->tensor(input)->dims;
-  int wanted_height = dims->data[1];
-  int wanted_width = dims->data[2];
-  int wanted_channels = dims->data[3];
-  tflite::label_image::resize<uint8_t>(
-    interpreter->typed_tensor<uint8_t>(input), input_data,
-    image_height, image_width, image_channels,
-    wanted_height, wanted_width, wanted_channels, &s);
-  cout << "OK: Image resized" << endl;
-
-  // Classify image
-  if (interpreter->Invoke() != kTfLiteOk) {
-    cerr << "ERROR: Failed to invoke tflite" << endl;
-    return -1;
-  }
-  cout << "OK: Image classified" << endl;
-
-  // Process results
-  const int output_size = 1000;
-  const size_t num_results = 5;
-  const float threshold = 0.001f;
-  vector<pair<float, int>> top_results;
-  tflite::label_image::get_top_n<uint8_t>(
-    interpreter->typed_output_tensor<uint8_t>(0),
-    output_size, num_results, threshold, &top_results, false);
-
-  // Read labels
-  vector<string> labels;
-  ifstream file(s.labels_file_name);
-  string line;
-  while (getline(file, line))
-    labels.push_back(line);
-
-  // Print predictions
-  for (const auto& result : top_results) {
-    const float confidence = result.first;
-    const int index = result.second;
-    cout << confidence << ": " << index << " " << labels[index] << endl;
-  }
-*/
   return 0;
 }
--- program/tensorflow-lite-classification/image_helper.h
+++ program/tensorflow-lite-classification/image_helper.h
@@ -66,26 +66,25 @@ ImageData load_jpeg_file(std::string file_name) {
 // Convert the eight-bit data in the image into float, resize
 // it using bilinear filtering, and scale it numerically to the float range that
 // the model expects (given by input_mean and input_std).
-std::vector<float> resize_image(const ImageData& img_data,
-                                int wanted_height, int wanted_width,
-                                float input_mean = 128, float input_std = 128) {
-  const int wanted_channels = img_data.channels;
-  std::vector<float> out(wanted_height * wanted_width * wanted_channels);
-  const size_t image_rowlen = img_data.width * img_data.channels;
-  const float width_scale = static_cast<float>(img_data.width) / wanted_width;
-  const float height_scale = static_cast<float>(img_data.height) / wanted_height;
+void resize_image(float* out, const ImageData& in,
+                  int wanted_height, int wanted_width,
+                  float input_mean = 128, float input_std = 128) {
+  const int wanted_channels = in.channels;
+  const size_t image_rowlen = in.width * in.channels;
+  const float width_scale = static_cast<float>(in.width) / wanted_width;
+  const float height_scale = static_cast<float>(in.height) / wanted_height;
   for (int y = 0; y < wanted_height; ++y) {
     const float in_y = y * height_scale;
     const int top_y_index = static_cast<int>(floorf(in_y));
-    const int bottom_y_index = std::min(static_cast<int>(ceilf(in_y)), (img_data.height - 1));
+    const int bottom_y_index = std::min(static_cast<int>(ceilf(in_y)), (in.height - 1));
     const float y_lerp = in_y - top_y_index; 
-    const uint8_t* in_top_row = img_data.data.data() + (top_y_index * image_rowlen);
-    const uint8_t* in_bottom_row = img_data.data.data() + (bottom_y_index * image_rowlen);
-    float *out_row = out.data() + (y * wanted_width * wanted_channels);
+    const uint8_t* in_top_row = in.data.data() + (top_y_index * image_rowlen);
+    const uint8_t* in_bottom_row = in.data.data() + (bottom_y_index * image_rowlen);
+    float *out_row = out + (y * wanted_width * wanted_channels);
     for (int x = 0; x < wanted_width; ++x) {
       const float in_x = x * width_scale;
       const int left_x_index = static_cast<int>(floorf(in_x));
-      const int right_x_index = std::min(static_cast<int>(ceilf(in_x)), (img_data.width - 1));
+      const int right_x_index = std::min(static_cast<int>(ceilf(in_x)), (in.width - 1));
       const uint8_t* in_top_left_pixel = in_top_row + (left_x_index * wanted_channels);
       const uint8_t* in_top_right_pixel = in_top_row + (right_x_index * wanted_channels);
       const uint8_t* in_bottom_left_pixel = in_bottom_row + (left_x_index * wanted_channels);
@@ -103,5 +102,4 @@ std::vector<float> resize_image(const ImageData& img_data,
       }
     }
   }
-  return out;
 }
