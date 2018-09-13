5b80596ed79c5cec89722e296120bfc029ffac61
Nikolay Chunosov <nikolay.chunosov@xored.com>
2018-09-13 16:38:41 +0700

    fix tf load order

--- program/image-classification-tf-py/classify.py
+++ program/image-classification-tf-py/classify.py
@@ -12,13 +12,6 @@ import json
 import os
 import sys
 import numpy as np
-
-# We have to augment search paths manually before importing TF
-# otherwise standard paths prepend ones from PYTHONPATH in `sys.path`
-# and pip-installed TF is loaded instead of ck-installed one
-for p in os.getenv('PYTHONPATH', '').split(os.pathsep):
-  sys.path.insert(0, p)
-
 import tensorflow as tf
 
 MODEL_MODULE = os.getenv('CK_ENV_TENSORFLOW_MODEL_MODULE')
