f605fcc121e2a134813ad8586fe6ce4d8a31c67e
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-03-04 21:43:53 +0000

    removed dependency on numpy and renamed

--- package/tflite-model-converted-resnet50/tf_2_tflite.py
+++ package/tflite-model-converted-resnet50/tf_2_tflite.py
@@ -9,9 +9,8 @@ output_tflite_model_filepath = sys.argv[2]
 # FIXME: keep them as magic constsants
 #
 input_arrays = ["input_tensor"]
-output_arrays = ["softmax_tensor", "ArgMax"]
+output_arrays = ["softmax_tensor","ArgMax"]
 
-converter = tf.contrib.lite.TFLiteConverter.from_frozen_graph(
-  input_tf_model_filepath, input_arrays, output_arrays)
+converter = tf.contrib.lite.TFLiteConverter.from_frozen_graph(input_tf_model_filepath, input_arrays, output_arrays)
 tflite_model = converter.convert()
 open(output_tflite_model_filepath, "wb").write(tflite_model)
