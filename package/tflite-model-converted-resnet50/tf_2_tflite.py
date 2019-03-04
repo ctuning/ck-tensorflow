ed3decfc2f5d75dd82cc4e20066cd8afcff12cca
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-03-04 22:23:10 +0000

    passing all parameters via environment

--- package/tflite-model-converted-resnet50/tf_2_tflite.py
+++ package/tflite-model-converted-resnet50/tf_2_tflite.py
@@ -1,16 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import sys
 import tensorflow as tf
 
-input_tf_model_filepath = sys.argv[1]
-output_tflite_model_filepath = sys.argv[2]
+def tf_2_tflite(input_ft_model_filepath, output_tflite_model_filepath, input_layer_names, output_layer_names):
+    "Perform the actual conversion from TF to TFlite"
 
-# FIXME: keep them as magic constsants
-#
-input_arrays = ["input_tensor"]
-output_arrays = ["softmax_tensor","ArgMax"]
+    converter = tf.contrib.lite.TFLiteConverter.from_frozen_graph(input_tf_model_filepath, input_layer_names, output_layer_names)
+    tflite_model = converter.convert()
+    open(output_tflite_model_filepath, "wb").write(tflite_model)
 
-converter = tf.contrib.lite.TFLiteConverter.from_frozen_graph(input_tf_model_filepath, input_arrays, output_arrays)
-tflite_model = converter.convert()
-open(output_tflite_model_filepath, "wb").write(tflite_model)
+
+if __name__ == '__main__':
+    import os
+
+    input_tf_model_filepath         = os.environ['CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH']
+    output_tflite_model_filepath    = os.path.join(os.environ['INSTALL_DIR'], os.environ['PACKAGE_NAME'])
+
+    # FIXME: ask the TF model what input and output layers it supports
+    #
+    input_layer_names = [ os.environ['MODEL_INPUT_LAYER_NAME'] ]
+    output_layer_names = [ os.environ['MODEL_OUTPUT_LAYER_NAME'] ]
+
+    tf_2_tflite(input_tf_model_filepath, output_tflite_model_filepath, input_layer_names, output_layer_names)
