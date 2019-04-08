6975afd45deea4b843a9bb40a2e57c1db6edfeb6
Ivan Osipov <bellycat77@gmail.com>
2019-04-08 15:51:01 +0700

    Fix for COCO2017 naming (#108)
    * Fix for COCO2017 naming
    
     - fix for program/object-detection-tf-py/ck_utils.py for
       dataset COCO2017 file naming
    
    * Add comments to ck_utils.py

--- program/object-detection-tf-py/ck_utils.py
+++ program/object-detection-tf-py/ck_utils.py
@@ -105,7 +105,12 @@ def filename_to_id(file_name, dataset_type):
 
   # In COCO dataset ID is a number which is a part of filename
   if dataset_type == COCO:
-    return int(re.split(r'_', short_name)[2])
+    # COCO 2017: 000000000139.jpg
+    # COCO 2014: COCO_val2014_000000000042.jpg
+    if short_name[0] == '0':
+      return int(short_name)
+    else:
+      return int(re.split(r'_', short_name)[2])
 
   # In OpenImages dataset image identifies by its name
   if dataset_type == OID:
