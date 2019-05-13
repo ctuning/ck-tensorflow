373d54d1953539120042cb8bc3170db10ee5a037
Ivan Osipov <bellycat77@gmail.com>
2019-05-13 10:50:00 +0700

    Fix bug in benchmark counters
     - add supporting alternative counters in benchmarks
     - add separate counter for NMS operation

--- program/object-detection-tflite/includes/benchmark.h
+++ program/object-detection-tflite/includes/benchmark.h
@@ -129,8 +129,9 @@ namespace CK {
 
         /// Begin measuring of new benchmark stage.
         /// Only one stage can be measured at a time.
-        void measure_begin() {
-            _start_time = std::chrono::high_resolution_clock::now();
+        void measure_begin(std::chrono::time_point<std::chrono::high_resolution_clock> *start_time=NULL) {
+            if (start_time == NULL) _start_time = std::chrono::high_resolution_clock::now();
+            else *start_time = std::chrono::high_resolution_clock::now();
         }
 
         /// Finish measuring of batch loading stage
@@ -155,14 +156,12 @@ namespace CK {
         }
 
         /// Finish measuring of non_max_suppression stage
-        float measure_end_non_max_suppression() {
-            float duration = measure_end();
+        float measure_end_non_max_suppression(std::chrono::time_point<std::chrono::high_resolution_clock> *start_time=NULL) {
+            float duration = measure_end(start_time);
             _total_prediction_time += duration;
             if (_settings->verbose())
                 std::cout << "non_max_suppression completed in " << duration << " s" << std::endl;
-            // Skip first batch in order to account warming-up the system
-            if (_batch_index > 0 || _settings->batch_count() == 1)
-                _non_max_suppression_time.add(duration);
+            _non_max_suppression_time.add(duration);
             return duration;
         }
 
@@ -180,9 +179,14 @@ namespace CK {
         std::vector<FileInfo> _batch_files;
         std::chrono::time_point<std::chrono::high_resolution_clock> _start_time;
 
-        float measure_end() const {
+        float measure_end(std::chrono::time_point<std::chrono::high_resolution_clock> *start_time=NULL) const {
             auto finish_time = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> elapsed = finish_time - _start_time;
+            std::chrono::duration<double> elapsed;
+            if (start_time == NULL) {
+                elapsed = finish_time - _start_time;
+            } else {
+                elapsed = finish_time - *start_time;
+            }
             return static_cast<float>(elapsed.count());
         }
     };
