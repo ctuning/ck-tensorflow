c0f96fe241465cfc7c6a967e875478e05b700caa
Ivan Osipov <bellycat77@gmail.com>
2019-07-15 17:41:42 +0700

    First commit for program:loadgen-image-classification-tflite

---
+++ program/loadgen-image-classification-tflite/README.md
@@ -0,0 +1,100 @@
+# TensorFlow Lite image classification program
+
+This program uses a statically linked [TensorFlow Lite](https://www.tensorflow.org/lite/) library.
+
+## Prerequisites
+
+### SciPy
+
+```
+# apt install liblapack-dev libatlas-dev
+# python -m pip install scipy
+```
+
+### TensorFlow library
+
+```
+$ ck install package:lib-tflite-0.1.7-src-static [--target_os=android23-arm64]
+```
+
+**NB:** Use `--target_os=android23-arm64` to build for Android API 23 (v6.0 "Marshmallow")
+or [similar](https://source.android.com/setup/start/build-numbers).
+
+### TensorFlow models
+
+Install a model providing a graph as tflite file e.g.:
+
+```
+$ ck install package:tensorflowmodel-mobilenet-v1-1.0-224-2018_02_22-py
+$ ck install package:tensorflowmodel-mobilenet-v2-1.0-224-py
+```
+
+### ImageNet dataset
+
+```
+$ ck install package --tags=small_dataset,imagenet,raw,val
+$ ck install package:imagenet-2012-aux 
+```
+
+## Build
+
+```
+$ ck compile program:image-classification-tflite [--target_os=android23-arm64]
+```
+
+## Run
+
+```
+$ ck run program:image-classification-tflite [--target_os=android23-arm64]
+```
+
+## Program parameters
+
+### Input image parameters
+
+#### `CK_IMAGE_FILE`
+
+If set, the program will classify a single image instead of iterating over a
+dataset. When only the name of an image is specified, it is assumed that the
+image is in the ImageNet dataset.
+
+```
+$ ck run program:image-classification-tflite --env.CK_IMAGE_FILE=/tmp/images/path-to-image.jpg
+$ ck run program:image-classification-tflite --env.CK_IMAGE_FILE=ILSVRC2012_val_00000011.JPEG
+```
+
+#### `CK_RECREATE_CACHE`
+If set to `YES`, then all previously cached images will be erased.
+
+Default: `NO`.
+
+### Input preprocessing parameters
+
+#### `CK_TMP_IMAGE_SIZE`
+
+The size of an intermediate image. If this preprocessing parameter is set to a
+value greater than the input image size defined by the model, input images
+will be first scaled to this size and then cropped to the input size.
+
+For example, if `--env.CK_TMP_IMAGE_SIZE=256` is specified for MobileNets
+models with the input image resolution of 224, then input images will be first
+resized to *256x256* and then cropped to *224x224*.
+
+Default: `0`.
+
+#### `CK_CROP_PERCENT`
+
+The percentage of the central image region to crop. If this preprocessing
+parameter is set to a value between 0 and 100, then loaded images will be
+cropped according this percentage and then scaled to the input image size
+defined by the model.
+
+Default: `87.5`.
+
+**NB:** If `CK_TMP_IMAGE_SIZE` is set and valid, this parameter is not used.
+
+#### `CK_SUBTRACT_MEAN`
+
+If set to `YES`, then the mean value will be subtracted from the input image.
+
+Default: `YES`.
---
+++ program/loadgen-image-classification-tflite/classification.cpp
@@ -0,0 +1,250 @@
+/*
+ * Copyright (c) 2018 cTuning foundation.
+ * See CK COPYRIGHT.txt for copyright details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause.
+ * See CK LICENSE.txt for licensing details.
+ */
+
+#include <future>
+
+#include "loadgen.h"
+#include "query_sample_library.h"
+#include "system_under_test.h"
+#include "test_settings.h"
+
+
+#include "benchmark.h"
+
+#ifdef TF_LITE_1_13
+  #include "tensorflow/lite/kernels/register.h"
+  #include "tensorflow/lite/model.h"
+#else
+  #include "tensorflow/contrib/lite/kernels/register.h"
+  #include "tensorflow/contrib/lite/model.h"
+#endif
+
+using namespace std;
+using namespace CK;
+
+
+template <typename TData, typename TInConverter, typename TOutConverter>
+class TFLiteBenchmark : public Benchmark<TData, TInConverter, TOutConverter> {
+public:
+  TFLiteBenchmark(const BenchmarkSettings* settings, tflite::Interpreter* interpreter, int input_index)
+    : Benchmark<TData, TInConverter, TOutConverter>(
+      settings, interpreter->typed_tensor<TData>(input_index), interpreter->typed_output_tensor<TData>(0)) {
+  }
+};
+
+class Program {
+public:
+  Program () {
+    init_benchmark();
+    
+    settings = new BenchmarkSettings(MODEL_TYPE::LITE);
+
+    // TODO: learn how to process batches via tflite.
+    // currently interpreter->tensor(input_index)->dims[0] = 1
+    if (settings->batch_size != 1)
+      throw string("Only BATCH_SIZE=1 is currently supported");
+    
+    session = new BenchmarkSession(settings);
+
+    cout << "\nLoading graph..." << endl;
+    measure_setup([&]{
+      model = tflite::FlatBufferModel::BuildFromFile(settings->graph_file().c_str());
+      if (!model)
+        throw "Failed to load graph from file " + settings->graph_file();
+
+      tflite::ops::builtin::BuiltinOpResolver resolver;
+      tflite::InterpreterBuilder(*model, resolver)(&interpreter);
+      if (!interpreter)
+        throw string("Failed to construct interpreter");
+      if (interpreter->AllocateTensors() != kTfLiteOk)
+        throw string("Failed to allocate tensors");
+
+      interpreter->SetNumThreads(settings->number_of_threads());
+
+      int input_index = interpreter->inputs()[0];
+      int output_index = interpreter->outputs()[0];
+      auto input_type = interpreter->tensor(input_index)->type;
+      auto output_type = interpreter->tensor(output_index)->type;
+      if (input_type != output_type)
+        throw format("Type of graph's input (%d) does not match type of its output (%d).",
+                     int(input_type), int(output_type));
+
+      switch (input_type) {
+        case kTfLiteFloat32:
+          if (settings->skip_internal_preprocessing)
+            benchmark.reset(new TFLiteBenchmark<float, InCopy, OutCopy>(settings, interpreter.get(), input_index));
+          else
+            benchmark.reset(new TFLiteBenchmark<float, InNormalize, OutCopy>(settings, interpreter.get(), input_index));
+          break;
+
+        case kTfLiteUInt8:
+          benchmark.reset(new TFLiteBenchmark<uint8_t, InCopy, OutDequantize>(settings, interpreter.get(), input_index));
+          break;
+
+        default:
+          throw format("Unsupported type of graph's input: %d. "
+                       "Supported types are: Float32 (%d), UInt8 (%d)",
+                       int(input_type), int(kTfLiteFloat32), int(kTfLiteUInt8));
+      }
+
+      TfLiteIntArray* in_dims = interpreter->tensor(input_index)->dims;
+      int in_num = in_dims->data[0];
+      int in_height = in_dims->data[1];
+      int in_width = in_dims->data[2];
+      int in_channels = in_dims->data[3];
+      cout << format("Input tensor dimensions (NHWC): %d*%d*%d*%d", in_num, in_height, in_width, in_channels) << endl;
+      if (in_height != settings->image_size ||
+          in_width != settings->image_size ||
+          in_channels != settings->num_channels)
+        throw format("Dimensions of graph's input do not correspond to dimensions of input image (%d*%d*%d*%d)",
+                     settings->batch_size, settings->image_size, settings->image_size, settings->num_channels);
+
+      TfLiteIntArray* out_dims = interpreter->tensor(output_index)->dims;
+      int out_num = out_dims->data[0];
+      int out_classes = out_dims->data[1];
+      cout << format("Output tensor dimensions: %d*%d", out_num, out_classes) << endl;
+      if (out_classes != settings->num_classes && out_classes != settings->num_classes+1)
+        throw format("Unsupported number of classes in graph's output tensor. Supported numbers are %d and %d",
+                     settings->num_classes, settings->num_classes+1);
+      benchmark->has_background_class = out_classes == settings->num_classes+1;
+    });
+  }
+
+  ~Program() {
+    finish_benchmark(*session);
+  }
+
+  bool is_available_batch() {return session? session->get_next_batch(): false; }
+
+  void LoadNextBatch() {
+    session->get_next_batch();
+    session->measure_begin();
+    benchmark->load_images(session->batch_files());
+    session->measure_end_load_images();
+  }
+
+  void InferenceBatch() {
+    session->measure_begin();
+
+    if (interpreter->Invoke() != kTfLiteOk)
+      throw "Failed to invoke tflite";
+
+    session->measure_end_prediction();
+  }
+
+  void UnloadBatch() {
+    benchmark->save_results(session->batch_files());
+  }
+
+  const int batch_size() {return settings->batch_size; }
+  const int batch_count() {return settings->batch_count; }
+
+private:
+  BenchmarkSettings *settings;
+  BenchmarkSession *session;
+  unique_ptr<IBenchmark> benchmark;
+  unique_ptr<tflite::Interpreter> interpreter;
+  unique_ptr<tflite::FlatBufferModel> model;
+};
+
+
+class SystemUnderTestSingleStream : public mlperf::SystemUnderTest {
+ public:
+  SystemUnderTestSingleStream(Program *prg) : mlperf::SystemUnderTest() {
+    this->prg = prg;
+  };
+  ~SystemUnderTestSingleStream() override = default;
+
+  const std::string& Name() const override { return name_; }
+
+  void IssueQuery(const std::vector<mlperf::QuerySample>& samples) override {
+    std::vector<mlperf::QuerySampleResponse> responses;
+    responses.reserve(samples.size());
+    for (auto s : samples) {
+      responses.push_back({s.id, 0, 0});
+    }
+    prg->InferenceBatch();
+    mlperf::QuerySamplesComplete(responses.data(), responses.size());
+  }
+
+  void FlushQueries() override {}
+  void ReportLatencyResults(
+      const std::vector<mlperf::QuerySampleLatency>& latencies_ns) override {}
+
+ private:
+  std::string name_{"SingleStreamSUT"};
+  Program *prg;
+};
+
+class QuerySampleLibrarySingleStream : public mlperf::QuerySampleLibrary {
+ public:
+  QuerySampleLibrarySingleStream(Program *prg) : mlperf::QuerySampleLibrary() {
+    this->prg = prg;
+  };
+  ~QuerySampleLibrarySingleStream() = default;
+
+  const std::string& Name() const override { return name_; }
+
+  const size_t TotalSampleCount() override { return prg->batch_count() * prg->batch_size(); }
+
+  const size_t PerformanceSampleCount() override { return prg->batch_size(); }
+
+  void LoadSamplesToRam( const std::vector<mlperf::QuerySampleIndex>& samples) override {
+    
+    prg->LoadNextBatch();
+    
+    return;
+  }
+
+  void UnloadSamplesFromRam( const std::vector<mlperf::QuerySampleIndex>& samples) override {
+    
+    prg->UnloadBatch();
+
+    return;
+  }
+
+ private:
+  std::string name_{"SingleStreamQSL"};
+  Program *prg;
+};
+
+void TestSingleStream(Program *prg) {
+  SystemUnderTestSingleStream sut(prg);
+  QuerySampleLibrarySingleStream qsl(prg);
+
+  mlperf::LogSettings log_settings;
+  log_settings.log_output.prefix_with_datetime = true;
+
+  mlperf::TestSettings ts;
+
+  mlperf::StartTest(&sut, &qsl, ts, log_settings);
+}
+
+int main(int argc, char* argv[]) {
+  try {
+    Program *prg = new Program();
+
+
+    measure_prediction([&]{
+      cout << "\nProcessing batches..." << endl;
+
+      // while (prg->is_available_batch()) {
+      //   prg->LoadNextBatch();
+      //   prg->InferenceBatch();
+      //   prg->UnloadBatch();
+      // }
+      TestSingleStream(prg);
+    });
+    delete prg;
+  }
+  catch (const string& error_message) {
+    cerr << "ERROR: " << error_message << endl;
+    return -1;
+  }
+  return 0;
+}
