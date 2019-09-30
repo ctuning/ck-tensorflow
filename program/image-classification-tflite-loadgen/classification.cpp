dd0813c870b341b6f9cd27258095f37a8ad406ab
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-09-30 16:55:26 +0100

    output latencies both in ns and fps in the end of the test

--- program/image-classification-tflite-loadgen/classification.cpp
+++ program/image-classification-tflite-loadgen/classification.cpp
@@ -193,25 +193,25 @@ public:
       const std::vector<mlperf::QuerySampleLatency>& latencies_ns) override {
 
     int size = latencies_ns.size();
-    std::vector<mlperf::QuerySampleLatency>  v(size);  
+    std::vector<mlperf::QuerySampleLatency> v(size);
     std::copy (latencies_ns.begin(), latencies_ns.end(), v.begin() );
 
-    cout << endl << "--------------------------------";
-    cout << endl << "|  LATENCIES (in nanoseconds)  |";
-    cout << endl << "--------------------------------";
-
+    cout << endl << "------------------------------------------------------------";
+    cout << endl << "|            LATENCIES (in nanoseconds and fps)            |";
+    cout << endl << "------------------------------------------------------------";
     sort(v.begin(), v.end());
     long avg = long(accumulate(v.begin(), v.end(), 0L))/size;
     int p50 = size * 0.5;
     int p90 = size * 0.9;
-    auto percentile50 = v[p50];
-    auto percentile90 = v[p90];
-    cout << endl << "Min latency: " << v[0];
-    cout << endl << "Max latency: " << v[size-1];
-    cout << endl << "Average latency: " << avg;
-    cout << endl << "Median latency: " << percentile50;
-    cout << endl << "90 percentile latency: " << percentile90;
-    cout << endl << "--------------------------------" << endl;
+    cout << endl << "Number of queries run: " << size;
+    cout << endl << "Min latency:                      " << v[0]            << "ns  (" << 1e9/v[0]            << " fps)";
+    cout << endl << "Median latency:                   " << v[p50]          << "ns  (" << 1e9/v[p50]          << " fps)";
+    cout << endl << "Average latency:                  " << avg             << "ns  (" << 1e9/avg             << " fps)";
+    cout << endl << "90 percentile latency:            " << v[p90]          << "ns  (" << 1e9/v[p90]          << " fps)";
+    cout << endl << "First query (cold model) latency: " << latencies_ns[0] << "ns  (" << 1e9/latencies_ns[0] << " fps)";
+    cout << endl << "Max latency:                      " << v[size-1]       << "ns  (" << 1e9/v[size-1]       << " fps)";
+
+    cout << endl << "------------------------------------------------------------ " << endl;
   }
 
 private:
