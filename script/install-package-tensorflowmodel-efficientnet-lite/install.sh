304c7bc9fe623b659798e4ce573e32592f4a9cee
Lahiru <lahiru.rasnayake@ntnu.no>
2020-07-31 12:35:16 +0200

    Fix install.sh

--- script/install-package-tensorflowmodel-efficientnet-lite/install.sh
+++ script/install-package-tensorflowmodel-efficientnet-lite/install.sh
@@ -7,9 +7,8 @@
 # SPDX-License-Identifier: BSD-3-Clause.
 # See CK LICENSE.txt for licensing details.
 #
-# MobileNet for TensorFlow
+# EfficientNet-Lite for TensorFlow
 # Python model and weights install script
-#
 
 ########################################################################
 echo
@@ -21,59 +20,23 @@ echo
 echo "Unpack weights file ${PACKAGE_NAME} ..."
 tar -zxvf ${PACKAGE_NAME}
 
-
-#Efficientnet-lite unpacks into subdirectory
-if [[ -d ${PACKAGE_NAME_EFFICIENTNET_LITE} ]]; then
-  echo
-  echo "Move files out of ${PACKAGE_NAME_EFFICIENTNET_LITE}/ ..."
-  mv ${PACKAGE_NAME_EFFICIENTNET_LITE}/* ${PACKAGE_NAME_EFFICIENTNET_LITE}/..
-  rmdir ${PACKAGE_NAME_EFFICIENTNET_LITE}
-
-  echo "Keeping file ${PACKAGE_NAME_EFFICIENTNET_LITE_WITH_PRECISION}.tflite"
-
-  if [ "${MODEL_EFFICIENTNET_LITE_PRECISION}" == "fp32" ]; then
-    file_to_remove="${PACKAGE_NAME_EFFICIENTNET_LITE}-int8.tflite"
-    echo "Removing file ${file_to_remove}"
-    rm ${file_to_remove}
-  else
-    file_to_remove="${PACKAGE_NAME_EFFICIENTNET_LITE}-fp32.tflite"
-    echo "Removing file ${file_to_remove}"
-    rm ${file_to_remove}
-  fi
-
-
+# EfficientNet-Lite unpacks into subdirectory
+echo
+echo "Move files out of ${PACKAGE_NAME_EFFICIENTNET_LITE}/ ..."
+mv ${PACKAGE_NAME_EFFICIENTNET_LITE}/* ${PACKAGE_NAME_EFFICIENTNET_LITE}/..
+rmdir ${PACKAGE_NAME_EFFICIENTNET_LITE}
+echo "Keeping file ${PACKAGE_NAME_EFFICIENTNET_LITE_WITH_PRECISION}.tflite"
+if [ "${MODEL_EFFICIENTNET_LITE_PRECISION}" == "fp32" ]; then
+  file_to_remove="${PACKAGE_NAME_EFFICIENTNET_LITE}-int8.tflite"
+  echo "Removing file ${file_to_remove}"
+  rm ${file_to_remove}
+else
+  file_to_remove="${PACKAGE_NAME_EFFICIENTNET_LITE}-fp32.tflite"
+  echo "Removing file ${file_to_remove}"
+  rm ${file_to_remove}
 fi
 
 ########################################################################
 echo
 echo "Remove temporary files ..."
 rm ${PACKAGE_NAME}
-
-function rm_file() {
-  if [ -f $1 ]; then
-    rm $1
-  fi
-}
-# We don't use it right now, so remove to save disk space, but it can be useful in future
-#rm_file "mobilenet_v${VERSION}_${MULTIPLIER}_${RESOLUTION}_eval.pbtxt"
-
-########################################################################
-echo
-echo "Copy Python modules ..."
-THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-#cp ${THIS_SCRIPT_DIR}/mobilenet-model.py .
-
-#if [ "${VERSION}" == "1" ]; then
-#  cp ${THIS_SCRIPT_DIR}/mobilenet_v1.py .
-#fi
-#if [ "${VERSION}" == "2" ]; then
-#  cp ${THIS_SCRIPT_DIR}/mobilenet_v2.py .
-#  cp ${THIS_SCRIPT_DIR}/mobilenet.py .
-#  cp ${THIS_SCRIPT_DIR}/conv_blocks.py .
-#fi
-#if [ "${VERSION}" == "3" ]; then
-#  cp ${THIS_SCRIPT_DIR}/mobilenet_v3.py .
-#  cp ${THIS_SCRIPT_DIR}/mobilenet.py .
-#  cp ${THIS_SCRIPT_DIR}/conv_blocks.py .
-#fi
